8ec71a8b9fa6b83b36064ed5195f46294e82f8ea
Edward Gilbert <egbot@asu.edu>
2018-12-03 11:15:13 -0700

    Create db_schema-1.0.sql

---
+++ neon/config/schema/db_schema-1.0.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `neon_biorepository`.`shipment` (
+  `shipmentPK` INT NOT NULL AUTO_INCREMENT,
+  `shipmentID` VARCHAR(25) NOT NULL,
+  `domainID` VARCHAR(10) NOT NULL,
+  `dateShipped` DATE NOT NULL,
+  `senderID` VARCHAR(45) NOT NULL,
+  `shipmentService` VARCHAR(45) NULL,
+  `shipmentMethod` VARCHAR(45) NULL,
+  `trackingNumber` VARCHAR(45) NULL,
+  `notes` VARCHAR(250) NULL,
+  `initialtimestamp` TIMESTAMP NULL DEFAULT current_timestamp,
+  PRIMARY KEY (`shipmentpk`));
+
+CREATE TABLE `neon_biorepository`.`samples` (
+  `samplePK` INT NOT NULL AUTO_INCREMENT,
+  `shipmentPK` INT NOT NULL,
+  `sampleID` VARCHAR(45) NULL,
+  `sampleClass` VARCHAR(45) NULL,
+  `namedlocation` VARCHAR(45) NULL,
+  `collectdate` DATE NULL,
+  `quarantineStatus` VARCHAR(4) NULL,
+  `initialtimestamp` TIMESTAMP NULL DEFAULT current_timestamp,
+  PRIMARY KEY (`samplePK`),
+  INDEX `FK_samples_shipmentid_idx` (`shipmentPK` ASC),
+  CONSTRAINT `FK_samples_shipmentid`
+    FOREIGN KEY (`shipmentPK`)
+    REFERENCES `neon_biorepository`.`shipment` (`shipmentPK`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);
+    

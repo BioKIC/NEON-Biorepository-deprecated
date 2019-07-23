a03c0e78530a5b2869b90bfa0f1951e419211558
Laura Rocha Prado <laurarochaprado@gmail.com>
2019-07-22 17:11:53 -0700

    implements draft for home mockup
    Adds skeleton.css for the grid and mockup images

--- index.php
+++ index.php
@@ -12,88 +12,95 @@ header("Content-Type: text/html; charset=".$CHARSET);
 	<title><?php echo $DEFAULT_TITLE; ?> Home</title>
 	<meta http-equiv="Expires" content="Tue, 01 Jan 1995 12:12:12 GMT">
 	<meta http-equiv="Pragma" content="no-cache">
-	<!-- CSS -->
-	<link href="css/base.cssver=<?php echo $CSS_VERSION; ?>" type="text/css" rel="stylesheet" />
-	<link href="css/main.css?ver=<?php echo $CSS_VERSION_LOCAL; ?>" type="text/css" rel="stylesheet" />
+	<!-- META AND CSS -->
+	<?php include_once($SERVER_ROOT.'/styles.php'); ?>
 	<link href="css/quicksearch.css" type="text/css" rel="Stylesheet" />
-
-	<!-- JS -->
 	<link href="js/jquery-ui-1.12.1/jquery-ui.min.css" type="text/css" rel="Stylesheet" />
-	<script src="js/jquery-3.2.1.min.js" type="text/javascript"></script>
+	<!-- JS -->
+<!-- 	<script src="js/jquery-3.2.1.min.js" type="text/javascript"></script>
 	<script src="js/jquery-ui-1.12.1/jquery-ui.min.js" type="text/javascript"></script>
-	<script src="js/symb/api.taxonomy.taxasuggest.js" type="text/javascript"></script>
+	<script src="js/symb/api.taxonomy.taxasuggest.js" type="text/javascript"></script> -->
 	<script type="text/javascript">
 		<?php include_once($SERVER_ROOT.'/config/googleanalytics.php'); ?>
 	</script>
 </head>
 <body>
-	<?php
-	include($SERVER_ROOT.'/header.php');
-	?>
+	<?php include($SERVER_ROOT.'/header.php'); ?>
 	<!-- This is inner text! -->
-	<div  id="innertext">
-		<!-- Intro -->
-		<div id="home-left">
-			<h2>Welcome to the NEON Biorepository Data Portal</h2>
-			<p>The National Ecological Observatory Network - NEON Biodiversity Data Portal is being developed to provide innovative access and discoverability to the entirety of NEON’s sample-based data products; including nearly 40 vertebrate, invertebrate, plant, microbial, and environmental sample collections. The majority of these samples are physically housed at the <a href="https://biokic.asu.edu/collections">Arizona State University Biocollections</a>, located in Tempe, Arizona.</p>
-			<p>The portal is being designed to allow current and prospective NEON researchers to interactively explore and understand sample availability and suitability for focal research interests, to initiate research sample loan requests, and to contribute and publish value-added sample data both directly in the portal and through emerging integration services with external data publishers.</p>
-			<p>The NEON Biorepository Data Portal is offered through the <a href="https://bdj.pensoft.net/articles.php?id=1114">Symbiota</a> software platform, and informationally synchronized with the main NEON Data Portal which serves the full spectrum of NEON data products. To learn more about the features and capabilities available through Symbiota, visit the <a href="http://symbiota.org/docs/">Symbiota Help Pages</a>. Join the portal as a regular visitor or contributor, and direct feedback or inquiries to <a href="mailto:BioRepo@asu.edu">BioRepo@asu.edu</a>. Visit the <a href="misc/usagepolicy.php">Data Usage Policy</a> page for information on how to cite data obtained from the NEON Biorepository Data Portal. Please consult the <a href="https://www.neonscience.org/data/archival-samples-specimens/neon-biorepository-asu">Archival Sample Request information page</a> to initiate inquiries about sample accessibility and loans.</p>
-			<p>The NEON Biorepository Data Portal currently serves over 65,000 samples and individual occurrence records (June 24, 2019).</p>
-		</div>
-		<div id="home-right">
-			<!-- Quick search -->
-			<div id="quicksearchdiv" >
-				<!-- -------------------------QUICK SEARCH SETTINGS--------------------------------------- -->
-				<form name="quicksearch" id="quicksearch" action="<?php echo $CLIENT_ROOT; ?>/taxa/index.php" method="get" onsubmit="return verifyQuickSearch(this);">
-					<div id="quicksearchtext" ><?php echo (isset($LANG['QSEARCH_SEARCH'])?$LANG['QSEARCH_SEARCH']:'Search Taxon'); ?></div>
-					<input id="taxa" type="text" name="taxon" />
-					<button name="formsubmit"  id="quicksearchbutton" type="submit" value="Search Terms"><?php echo (isset($LANG['QSEARCH_SEARCH_BUTTON'])?$LANG['QSEARCH_SEARCH_BUTTON']:'Search'); ?></button>
-				</form>
+	<div id="innertext" class="container" style="margin-top: 5%">
+	    <h1 class="centered">Discover and access NEON sample-based data</h1>
+
+	    <div class="row">
+			<div class="four columns">
+			    <h4 class="centered">Samples</h4>
+			    <img src="https://imgplaceholder.com/420x120/cccccc/757575/glyphicon-stats" alt="ImgPlaceholder">
 			</div>
-			<?php
-			//---------------------------GAME SETTINGS---------------------------------------
-			//If more than one game will be active, assign unique numerical ids for each game.
-			//If only one game will be active, leave set to 1.
-			$oodID = 1;
+			<div class="four columns">
+			    <h4 class="centered">Taxa</h4>
+			    <img src="https://imgplaceholder.com/420x120/cccccc/757575/glyphicon-stats" alt="ImgPlaceholder">
+			</div>	      
+			<div class="four columns">
+			    <h4 class="centered">Preservation methods</h4>
+			    <img src="https://imgplaceholder.com/420x120/cccccc/757575/glyphicon-list" alt="ImgPlaceholder">
+			</div>      
+	    </div>
 
-			//Enter checklist id (clid) of the checklist you wish to use, if you would like to use more than one checklist,
-			//separate their ids with a comma ex. "1,2,3,4"
-			$ootdGameChecklist = "98";
+	    <div class="row">	    
+	    	<div class="six columns">
+	    		<h2 class="centered">About the portal</h2>
+	    		<p>The NEON Biorepository data portal allows one to</p>
+	    		<ul>
+	    			<li>Discover sample availability and suitability for focal research interests</li>
+	    			<li>Initiate sample loan requests</li>
+	    			<li>Contribute and publish value-added sample data</li>
+	    		</ul>
+	    		<p>The majority of the samples published here are physically housed at the <a href="https://biokic.asu.edu/collections">Arizona State University Biocollections</a>, located in Tempe, Arizona.</p>
+	    	</div>
+	    	<div class="six columns">
+	    		<h2 class="centered">Learn more</h2>
+	    		<p>Description</p>
+	    		<p>Neon/Batelle</p>
+	    		<p>Tutorials</p>
+	    		<p>Contact</p>	
+	    	</div>
+	    </div>
 
-			//Change to modify title
-			$ootdGameTitle = "Organism of the Day ";
+	    <div class="row  centered">
+	    	<h2>Discover</h2>
+	    	<div class="three columns centered" style="background-color:#cccccc">
+	    		<a href="https://imgplaceholder.com">
+	    			<img src="https://imgplaceholder.com/100x100/cccccc/757575/glyphicon-search" alt="ImgPlaceholder">
+	    			<p style="text-decoration: none;font-size:1.5rem;text-transform:uppercase">Text search</p>
+	    		</a>
+	    	</div>
+	    	<div class="three columns centered" style="background-color:#cccccc">
+	    		<a href="https://imgplaceholder.com">
+	    			<img src="https://imgplaceholder.com/100x100/cccccc/757575/glyphicon-globe" alt="ImgPlaceholder">
+	    			<p style="text-decoration: none;font-size:1.5rem;text-transform:uppercase;background-color:#cccccc">Map search</p>
+	    		</a>
+	    	</div>
+	    	<div class="three columns centered" style="background-color:#cccccc">
+	    		<a href="https://imgplaceholder.com">
+	    			<img src="https://imgplaceholder.com/100x100/cccccc/757575/glyphicon-list-alt" alt="ImgPlaceholder">
+	    			<p style="text-decoration: none;font-size:1.5rem;text-transform:uppercase;background-color:#cccccc">Species lists</p>
+	    		</a>
+	    	</div>
+	    	<div class="three columns centered" style="background-color:#cccccc">
+	    		<a href="https://imgplaceholder.com">
+	    			<img src="https://imgplaceholder.com/100x100/cccccc/757575/glyphicon-tree-deciduous" alt="ImgPlaceholder">
+	    			<p style="text-decoration: none;font-size:1.5rem;text-transform:uppercase;background-color:#cccccc">Taxonomic tree</p>
+	    		</a>
+	    	</div>	    		    		    	
+	    </div>
 
-			//Replace "plant" with the type of organism, eg: plant, animal, insect, fungi, etc.
-			//This setting will appear in "Name that ______"
-			$ootdGameType = "plant";
-			//---------------------------DO NOT CHANGE BELOW HERE-----------------------------
+	    <div class="row">
+		    <h2 class="centered">Access</h2>
+	    	<div class="four columns"><p>Visit the <a href="misc/usagepolicy.php">Data Usage Policy</a> page for information on how to cite data obtained from the NEON Biorepository Data Portal.</p></div>
+	    	<div class="four columns"><p>Please consult the <a href="https://www.neonscience.org/data/archival-samples-specimens/neon-biorepository-asu">Archival Sample Request information page</a> to initiate inquiries about sample accessibility and loans.</p></div>
+	    	<div class="four columns"><p>Join the portal as a regular visitor or contributor, and direct feedback or inquiries to <a href="mailto:BioRepo@asu.edu">BioRepo@asu.edu</a>.</p></div>
+	    </div>
 
-			include_once($SERVER_ROOT.'/classes/GamesManager.php');
-			$gameManager = new GamesManager();
-			$gameInfo = $gameManager->setOOTD($oodID,$ootdGameChecklist);
-			?>
-			<div>
-				<div style="font-size:130%;font-weight:bold;">
-					<?php echo $ootdGameTitle; ?>
-				</div>
-				<a href="<?php echo $CLIENT_ROOT; ?>/games/ootd/index.php?oodid=<?php echo $oodID.'&cl='.$ootdGameChecklist.'&title='.$ootdGameTitle.'&type='.$ootdGameType; ?>">
-					<img src="<?php echo $gameInfo['images'][0]; ?>" style="width:250px;border:0px;" />
-				</a><br/>
-				<b>What is this <?php echo $ootdGameType; ?>?</b><br/>
-				<a href="<?php echo $CLIENT_ROOT; ?>/games/ootd/index.php?oodid=<?php echo $oodID.'&cl='.$ootdGameChecklist.'&title='.$ootdGameTitle.'&type='.$ootdGameType; ?>">
-					Click here to test your knowledge
-				</a>
-			</div>
-
-
-		</div>
-
-
-
-		</div>
-
-	<?php
-	include($SERVER_ROOT.'/footer.php');
-	?>
+	</div>
+	<?php include($SERVER_ROOT.'/footer.php'); ?>
 </body>
 </html>
---
+++ styles.php
@@ -0,0 +1,13 @@
+<!-- Mobile Specific Metas
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- FONT
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700" rel="stylesheet" type="text/css">
+<!-- UNIVERSAL CSS
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="css/skeleton.css">
+<link rel="stylesheet" href="css/base.css?ver=<?php echo $CSS_VERSION; ?>">
+<link rel="stylesheet" href="css/neon.css?ver=<?php echo $CSS_VERSION_LOCAL; ?>">
+<link rel="stylesheet" href="css/main.css?ver=<?php echo $CSS_VERSION_LOCAL; ?>">

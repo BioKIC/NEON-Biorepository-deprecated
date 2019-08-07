c1fda122f3c16e68f9d1a5505f23e7c868f8d0b2
Laura Rocha Prado <laurarochaprado@gmail.com>
2019-08-06 17:02:15 -0700

    Creates pages for sample usage guidelines

---
+++ misc/samplepolicy.php
@@ -0,0 +1,29 @@
+<?php
+include_once('../config/symbini.php');
+header("Content-Type: text/html; charset=".$CHARSET);
+?>
+<html>
+	<head>
+		<title>Sample Use Policy</title>
+		<link href="<?php echo $CLIENT_ROOT; ?>/css/base.css?ver=<?php echo $CSS_VERSION; ?>" type="text/css" rel="stylesheet" />
+		<link href="<?php echo $CLIENT_ROOT; ?>/css/main.css<?php echo (isset($CSS_VERSION_LOCAL)?'?ver='.$CSS_VERSION_LOCAL:''); ?>" type="text/css" rel="stylesheet" />
+	</head>
+	<body>
+		<?php
+		$displayLeftMenu = true;
+		include($SERVER_ROOT.'/header.php');
+		?>
+		<div class="navpath">
+			<a href="<?php echo $CLIENT_ROOT; ?>/index.php">Home</a> &gt;&gt;
+			<b>Sample Use Policy</b>
+		</div>
+		<!-- This is inner text! -->
+		<div id="innertext">
+			<h1>Sample Use Policy</h1>
+
+		</div>
+		<?php
+			include($SERVER_ROOT.'/footer.php');
+		?>
+	</body>
+</html>

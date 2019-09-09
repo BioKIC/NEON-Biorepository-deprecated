bab2303863e8e3dcd2fed15ce82409e5e16bc2ac
Edward Gilbert <egbot@asu.edu>
2019-09-08 19:50:38 -0700

    Paleo Module Development
    - Make parent time fields auto-populate with appropriate terms
    - Term adjustments: remove some terms and add others
    - Misc bug fixes and layout adjustments

---
+++ collections/editor/rpc/getPaleoGtsParents.php
@@ -0,0 +1,12 @@
+<?php
+include_once('../../../config/symbini.php');
+include_once($SERVER_ROOT.'/classes/OccurrenceEditorServices.php');
+header("Content-Type: application/json; charset=".$CHARSET);
+
+$term = $_REQUEST['term'];
+
+$searchManager = new OccurrenceEditorServices();
+$retArr = $searchManager->getPaleoGtsParents($term);
+
+echo json_encode($retArr);
+?>

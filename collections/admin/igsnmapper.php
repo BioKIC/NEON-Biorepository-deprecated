c5c47525075f7cbf00447cb3022d8b3fada1257b
Edward Gilbert <egbot@asu.edu>
2019-10-10 00:35:04 -0700

    IGSN Management Dev
    - Add control panel forms and control field for registering samples for IGSN assignments
    - Add jquery user validation function
    - General prep for adding tools as a main menu function
    - Minor error within GBIF publishing tools

--- collections/admin/igsnmapper.php
+++ collections/admin/igsnmapper.php
@@ -7,6 +7,8 @@ ini_set('max_execution_time', 3600);
 if(!$SYMB_UID) header('Location: ../../profile/index.php?refurl=../collections/admin/igsndmapper.php?'.$_SERVER['QUERY_STRING']);
 
 $collid = array_key_exists('collid',$_REQUEST)?$_REQUEST['collid']:0;
+$username = array_key_exists('username',$_REQUEST)?$_REQUEST['username']:'';
+$pwd = array_key_exists('pwd',$_REQUEST)?$_REQUEST['pwd']:'';
 $action = array_key_exists('formsubmit',$_POST)?$_POST['formsubmit']:'';
 
 $isEditor = 0;
@@ -22,45 +24,57 @@ $guidManager->setCollid($collid);
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $CHARSET; ?>">
 	<title>IGSN GUID Management</title>
 	<link rel="stylesheet" href="../../css/base.css?ver=<?php echo $CSS_VERSION; ?>" type="text/css" />
-    <link rel="stylesheet" href="../../css/main.css<?php echo (isset($CSS_VERSION_LOCAL)?'?ver='.$CSS_VERSION_LOCAL:''); ?>" type="text/css" />
+	<link rel="stylesheet" href="../../css/main.css<?php echo (isset($CSS_VERSION_LOCAL)?'?ver='.$CSS_VERSION_LOCAL:''); ?>" type="text/css" />
+	<script type="text/javascript" src="../../js/jquery.js"></script>
 	<script type="text/javascript">
-		function toggle(target){
-			var objDiv = document.getElementById(target);
-			if(objDiv){
-				if(objDiv.style.display=="none"){
-					objDiv.style.display = "block";
+		function validateCredentials(f){
+			if(f.username.value == "" || f.pwd.value == ""){
+				alert("Please enter valid SESAR username and password");
+				return false;
+			}
+			$.ajax({
+				method: "POST",
+				data: { username: f.username.value, password: f.pwd.value },
+				dataType: "xml",
+				url: "https://app.geosamples.org/webservices/credentials_service_v2.php"
+			})
+			.done(function(xml) {
+				var valid = $(xml).find('valid').text();
+				if(valid == "yes"){
+					alert("valid user");
+					$(xml).find('user_codes').each(function(){
+	                    $(this).find("user_code").each(function(){
+	                        var userCode = $(this).text();
+	                        alert(userCode);
+	                        $("#igsn-reg-div").show();
+	                    });
+	                });
 				}
 				else{
-					objDiv.style.display = "none";
+					alert($(xml).find('error').text());
+                    $("#igsn-reg-div").hide();
 				}
-			}
-			else{
-			  	var divs = document.getElementsByTagName("div");
-			  	for (var h = 0; h < divs.length; h++) {
-			  	var divObj = divs[h];
-					if(divObj.className == target){
-						if(divObj.style.display=="none"){
-							divObj.style.display="block";
-						}
-					 	else {
-					 		divObj.style.display="none";
-					 	}
-					}
-				}
-			}
-			return false;
+			})
+			.fail(function() {
+				alert("Validation call failed");
+			});
 		}
 
 		function verifyGuidForm(f){
 
 			return true;
-    	}
+		}
 
 		function verifyGuidAdminForm(f){
 
 			return true;
-    	}
-    </script>
+		}
+	</script>
+	<style type="text/css">
+		fieldset{ margin:10px }
+		.form-label{ font-weight: bold; }
+		button{ margin:15px; }
+	</style>
 </head>
 <body>
 <?php
@@ -85,16 +99,64 @@ include($SERVER_ROOT."/header.php");
 
 		if($collid) echo '<h3>'.$guidManager->getCollectionName().'</h3>';
 		?>
-		<div style="font-weight:bold;">Records without GUIDs (UUIDs)</div>
-		<div style="margin:10px;">
-			<div><b>Occurrences: </b><?php echo $guidManager->getMissingGuidCount(); ?></div>
-		</div>
+		<p>
+			<b>Occurrences without GUIDs:</b> <?php echo $guidManager->getMissingGuidCount(); ?>
+		</p>
 		<form name="guidform" action="igsnmapper.php" method="post" onsubmit="return verifyGuidForm(this)">
-			<fieldset style="padding:15px;">
-				<legend><b>GUID (UUID) Mapper</b></legend>
-				<div style="clear:both;">
+			<fieldset>
+				<legend>Manual Registration</legend>
+				<p>Generate registration documents for manual submittions</p>
+				<p>
+					<span class="form-label">Registration method:</span>
+					<select name="registrationMethod">
+						<option value=''>-- Select Method --</option>
+						<option value=''>----------------------------</option>
+						<option value='csv'>Batch CSV submission</option>
+						<option value='xml'>Batch XML submission</option>
+					</select>
+				</p>
+				<p>
+					<span class="form-label">IGSN generation method:</span>
+					<select name="generationMethod">
+						<option value=''>-- Select Method --</option>
+						<option value=''>----------------------------</option>
+						<option value='sesar'>SESAR generates IGSN (recommended)</option>
+						<option value='inhouse'>Generate IGSN in-house</option>
+					</select>
+				</p>
+				<p>
 					<input type="hidden" name="collid" value="<?php echo $collid; ?>" />
-					<input type="submit" name="formsubmit" value="Populate Collection GUIDs" />
+					<button type="submit" name="formsubmit" value="populateGUIDs">Populate Collection GUIDs</button>
+				</p>
+			</fieldset>
+		</form>
+		<form name="guidform" action="igsnmapper.php" method="post" onsubmit="return verifyGuidForm(this)">
+			<fieldset>
+				<legend>Automatic Registration</legend>
+				<p>Register IGSN automatically using the <a href="http://www.geosamples.org/interop" target="_blank">SESAR API Services</a></p>
+				<p>
+					<div><span class="form-label">Username:</span> <input name="username" type="text" value="<?php echo $username; ?>" /></div>
+					<div><span class="form-label">Password:</span> <input name="pwd" type="password" value="<?php echo $pwd; ?>" /></div>
+					<button type="button" onclick="validateCredentials(this.form)">Validate Credentials</button>
+				</p>
+				<div id="igsn-reg-div" style="display:none">
+					<p>
+						<span class="form-label">IGSN generation method:</span>
+						<select name="generationMethod">
+							<option value=''>-- Select Method --</option>
+							<option value=''>----------------------------</option>
+							<option value='sesar'>SESAR generates IGSN (recommended)</option>
+							<option value='inhouse'>Generate IGSN in-house</option>
+						</select>
+					</p>
+					<p>
+						<span class="form-label">Number of identifiers to generate: </span>
+						<input name="count" type="text" value="10" /> (leave blank to process all specimens)
+					</p>
+					<p>
+						<input type="hidden" name="collid" value="<?php echo $collid; ?>" />
+						<button type="submit" name="formsubmit" value="populateGUIDs">Populate Collection GUIDs</button>
+					</p>
 				</div>
 			</fieldset>
 		</form>

5b8f474324c90901d94fe4a984adcd4529c01b5e
Laura Rocha Prado <lauraprado@asu.edu>
2020-02-18 10:50:43 -0700

    Adapts `collections/cleaning` files to new framework structure

--- collections/cleaning/fieldstandardization.php
+++ collections/cleaning/fieldstandardization.php
@@ -37,11 +37,20 @@ if($collMap['colltype'] == 'General Observations' && $obsUid !== 0){
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $CHARSET; ?>">
 	<title><?php echo $DEFAULT_TITLE; ?> Field Standardization</title>
-	<link href="../../css/base.css?ver=<?php echo $CSS_VERSION; ?>" type="text/css" rel="stylesheet" />
-    <link href="../../css/main.css<?php echo (isset($CSS_VERSION_LOCAL)?'?ver='.$CSS_VERSION_LOCAL:''); ?>" type="text/css" rel="stylesheet" />
-	<script type="text/javascript">
+  <?php
+    $activateJQuery = false;
+    if(file_exists($SERVER_ROOT.'/includes/head.php')){
+      include_once($SERVER_ROOT.'/includes/head.php');
+    }
+    else{
+      echo '<link href="'.$CLIENT_ROOT.'/css/jquery-ui.css" type="text/css" rel="stylesheet" />';
+      echo '<link href="'.$CLIENT_ROOT.'/css/base.css?ver=1" type="text/css" rel="stylesheet" />';
+      echo '<link href="'.$CLIENT_ROOT.'/css/main.css?ver=1" type="text/css" rel="stylesheet" />';
+    }
+  ?>
+  <script type="text/javascript">
 
-	</script>
+  </script>
 </head>
 <body>
 	<?php

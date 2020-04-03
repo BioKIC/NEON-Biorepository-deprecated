96020fd622cfd0fad3830891b4eb637b3fdb1f12
Edward Gilbert <egbot@asu.edu>
2020-04-03 14:52:55 -0700

    CCH configuration files

---
+++ includes/head.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Customize css links below as needed to establish custom look-and-feel for portal
+ */
+
+?>
+<meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />
+<?php
+if($activateJQuery){
+	?>
+	<link href="<?php echo $CLIENT_ROOT; ?>/css/jquery-ui.css" type="text/css" rel="stylesheet">
+	<?php
+}
+?>
+<link href="<?php echo $CLIENT_ROOT; ?>/css/base.css?ver=1" type="text/css" rel="stylesheet">
+<link href="<?php echo $CLIENT_ROOT; ?>/css/main.css?ver=1" type="text/css" rel="stylesheet">
+

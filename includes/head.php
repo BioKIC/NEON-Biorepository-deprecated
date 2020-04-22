f7dec15075a50d50f3d70080430aef012bcfd627
Edward Gilbert <egbot@asu.edu>
2020-04-22 13:12:22 -0700

    adjustmetns to head.php include

--- includes/head.php
+++ includes/head.php
@@ -2,16 +2,16 @@
 /*
  * Customize css links below as needed to establish custom look-and-feel for portal
  */
-
+//Customized styling: copy all /css/symb/*.css files into alternate directory (e.g. /css/symb/custom) and then modify $cssPathPrefix to point into that directory
+$cssPathPrefix = $CLIENT_ROOT.'/css/symb';
+//$cssPathPrefix = $CLIENT_ROOT.'/css/symb/custom';
 ?>
 <meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />
 <?php
 if($activateJQuery){
-	?>
-	<link href="<?php echo $CLIENT_ROOT; ?>/css/jquery-ui.css" type="text/css" rel="stylesheet">
-	<?php
+	//For an alternate jQuery UI styling, point link below to another css file
+	echo '<link href="'.$CLIENT_ROOT.'/css/jquery-ui.css" type="text/css" rel="stylesheet">';
 }
 ?>
 <link href="<?php echo $CLIENT_ROOT; ?>/css/base.css?ver=1" type="text/css" rel="stylesheet">
 <link href="<?php echo $CLIENT_ROOT; ?>/css/main.css?ver=1" type="text/css" rel="stylesheet">
-

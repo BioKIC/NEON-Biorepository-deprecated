55155a9e0b3f80d7ea7a151b1f20cbeef28c3686
Laura Rocha Prado <lauraprado@asu.edu>
2020-05-28 14:51:16 -0700

    adds tutorial images

---
+++ misc/searchtutorial.php
@@ -0,0 +1,40 @@
+<?php
+include_once('../config/symbini.php');
+header("Content-Type: text/html; charset=".$CHARSET);
+?>
+<html>
+	<head>
+		<title>Conduct a Sample Search</title>
+    <?php
+      $activateJQuery = false;
+      if(file_exists($SERVER_ROOT.'/includes/head.php')){
+        include_once($SERVER_ROOT.'/includes/head.php');
+      }
+      else{
+        echo '<link href="'.$CLIENT_ROOT.'/css/jquery-ui.css" type="text/css" rel="stylesheet" />';
+        echo '<link href="'.$CLIENT_ROOT.'/css/base.css?ver=1" type="text/css" rel="stylesheet" />';
+        echo '<link href="'.$CLIENT_ROOT.'/css/main.css?ver=1" type="text/css" rel="stylesheet" />';
+      }
+    ?>
+	</head>
+	<body>
+		<?php
+		$displayLeftMenu = true;
+		include($SERVER_ROOT.'/includes/header.php');
+		?>
+		<div class="navpath">
+      <a href="<?php echo $CLIENT_ROOT; ?>/index.php">Home</a> &gt;&gt;
+      <a href="<?php echo $CLIENT_ROOT; ?>/misc/tutorials.php">Tutorials</a> &gt;&gt;   
+			<b>View the Homepage</b>
+		</div>
+		<!-- This is inner text! -->
+		<div id="innertext">
+<h1 style="text-align: center;">Tutorials and Help</h1>
+
+
+		</div>
+		<?php
+			include($SERVER_ROOT.'/includes/footer.php');
+		?>
+	</body>
+</html>

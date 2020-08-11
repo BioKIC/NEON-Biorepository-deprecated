3d0f1d4a4e0101ff02baaaa2f77a3836f2596c94
Andrew Johnston <ajohnston@asu.edu>
2020-08-10 17:22:24 -0700

    Create index.en.php

---
+++ content/lang/index.en.php
@@ -0,0 +1,24 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+//Taxon Quick Search plugin variables
+$LANG['QSEARCH_SEARCH'] = 'Search Taxon';
+$LANG['QSEARCH_SEARCH_BUTTON'] = 'Search';
+
+//Slideshow plugin variables
+$LANG['HIDE_CAPTION'] = 'HIDE CAPTION';
+$LANG['SHOW_CAPTION'] = 'SHOW CAPTION';
+$LANG['IMAGE_BY'] = 'Image by';
+$LANG['COURTESY_OF'] = 'Courtesy of';
+
+$LANG['LEFT_TITLE'] = '';
+$LANG['LEFT_TEXT'] = '';
+$LANG['RIGHT_TITLE'] = '';
+$LANG[RIGHT_TEXT'] = '';
+
+
+?>

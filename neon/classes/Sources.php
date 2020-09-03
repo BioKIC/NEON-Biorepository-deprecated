6155489f262c12274846c44a0690c40b0dcebcfb
Laura Rocha Prado <lauraprado@asu.edu>
2020-09-02 18:12:40 -0700

    added class-based taxonomies page
    Refactored from using mock API + JS to pure PHP

---
+++ neon/classes/Sources.php
@@ -0,0 +1,96 @@
+<?php 
+include_once($SERVER_ROOT.'/classes/Manager.php');
+
+/**
+ * Controler class for /neon/classes/Search.php
+ * 
+ */
+
+class Sources extends Manager { 
+
+	// private $variable1;
+	// private $variable2;
+	
+	public function __construct(){
+    parent::__construct(null,'readonly');
+    $this->verboseMode = 2;
+		set_time_limit(2000);
+	}
+
+	public function __destruct(){
+ 		parent::__destruct();
+	}
+	
+  //Main functions
+  
+  // Gets a list of sources from occurrences by collection
+  public function getOccSourcesByColl(){
+    $dataArr = array();
+
+    $sql = 'SELECT category, collectionname, source FROM taxsourcescoll ORDER BY category, collectionname';
+
+    $result = $this->conn->query($sql);
+
+    if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()){
+      $dataArr[] = $row;
+    }
+    return $dataArr;
+    }
+    else {
+      http_response_code(403);
+    }
+   $conn->close();
+  }
+
+  // Gets a list of unique sources from occurrences
+  public function getUniqueOccSources(){
+    $dataArr = array();
+
+    $sql = 'SELECT DISTINCT source FROM taxsourcescoll ORDER BY source';
+
+    $result = $this->conn->query($sql);
+
+    if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()){
+      $dataArr[] = $row;
+    }
+    return $dataArr;
+    }
+    else {
+      http_response_code(403);
+    }
+   $conn->close();
+
+  }
+	
+  // Formats array in tabular form (pass array name and headers array as arguments)
+  public function htmlTable($data, $headerArr){
+    foreach ($headerArr as $header){
+      $headers[] = "<th>{$header}</th>";
+    }
+    $rows = array();
+    foreach ($data as $row) {
+        $cells = array();
+        foreach ($row as $cell) {
+            $cells[] = "<td>{$cell}</td>";
+        }
+        $rows[] = "<tr>" . implode('', $cells) . "</tr>";
+    }
+    return "<table>" . implode('', array_merge($headers, $rows)) . "</table>";
+  }
+	
+	//Setters and getters
+	// public function setGeneralVariable($var){
+	// 	$this->variable1 = $this->cleanInStr($var);
+	// }
+	
+	// public function setNumericVariable($var){
+	// 	if(is_numeric($var)){
+	// 		$this->variable2 = $var;
+	// 	}
+	// }
+} 
+?>

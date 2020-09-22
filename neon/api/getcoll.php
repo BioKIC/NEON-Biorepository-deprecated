ae8f62371e67834782b8ebc483bfc310ebec78ca
Laura Rocha Prado <lauraprado@asu.edu>
2020-09-22 14:15:11 -0700

    adds view-based api output for colls metadata

--- neon/api/getcoll.php
+++ neon/api/getcoll.php
@@ -1,24 +1,30 @@
 <?php 
-
   include_once('../../config/symbini.php');
-  include_once('../../config/dbconnection.php');
+  include_once($SERVER_ROOT.'/neon/classes/CollectionMetadata.php');
+  header("Content-Type: text/html; charset=".$CHARSET);
 
-  $conn = MySQLiConnectionFactory::getCon('readonly');
-  $id = ((array_key_exists("collid",$_REQUEST) && is_numeric($_REQUEST["collid"]))?$_REQUEST["collid"]:0);
-  $sql = 'SELECT collid, institutioncode, collectionname, datasetname, fulldescription, homepage, dwcaurl FROM omcollections WHERE collid='.$id.'';
-  $sql = $conn->real_escape_string($sql);
-  $result = $conn->query($sql);
-  $dataArr = array();
+  $coll = new CollectionMetadata();
 
-  if ($id && $result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()){
-      $dataArr[] = $row;
-    }
-    echo json_encode($dataArr);
-    }
-    else {
-      http_response_code(403);
-    }
-   $conn->close();
+  $collInfo = $coll->getCollMetaById(50);
+
+  echo json_encode($collInfo);
+
+  // $conn = MySQLiConnectionFactory::getCon('readonly');
+  // $id = ((array_key_exists("collid",$_REQUEST) && is_numeric($_REQUEST["collid"]))?$_REQUEST["collid"]:0);
+  // $sql = 'SELECT collid, institutioncode, collectionname, datasetname, fulldescription, homepage, dwcaurl FROM omcollections WHERE collid='.$id.'';
+  // $sql = $conn->real_escape_string($sql);
+  // $result = $conn->query($sql);
+  // $dataArr = array();
+
+  // if ($id && $result->num_rows > 0) {
+  //   // output data of each row
+  //   while($row = $result->fetch_assoc()){
+  //     $dataArr[] = $row;
+  //   }
+  //   echo json_encode($dataArr);
+  //   }
+  //   else {
+  //     http_response_code(403);
+  //   }
+  //  $conn->close();
 ?>
---
+++ neon/classes/CollectionMetadata.php
@@ -0,0 +1,47 @@
+<?php 
+
+  include_once($SERVER_ROOT.'/classes/Manager.php');
+
+  /**
+ * Controler class for /neon/classes/CollectionMetadata.php
+ * 
+ */
+
+ class CollectionMetadata extends Manager {
+   
+  public function __construct() {
+    parent::__construct(null,'readonly');
+    $this->verboseMode = 2;
+    set_time_limit(2000);
+  }
+
+  public function __destruct() {
+    parent::__destruct();
+  }
+
+  // Main functions
+
+  // Gets individual collection profile metadata with collid
+  public function getCollMetaById($collid){
+    $dataArr = array();
+
+    $sql = 'SELECT * FROM collmetadata WHERE collid = '.$collid.'';
+
+    $result = $this->conn->query($sql);
+
+    if ($result->num_rows > 0) {
+      //output data of each row
+      while ($row = $result->fetch_assoc()){
+        $dataArr[] = $row;
+      }
+      $result->free();
+    } 
+    else {
+      $this->errorMessage = 'Collection Metadata query was not successfull';
+      $dataArr = false;
+    }
+    return $dataArr;
+  }
+}
+
+;?>

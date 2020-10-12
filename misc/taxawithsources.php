78d5a7dad6c694ff696147f3d9a7137584a1e520
Laura Rocha Prado <lauraprado@asu.edu>
2020-10-12 15:48:10 -0700

    fixes limits

--- misc/taxawithsources.php
+++ misc/taxawithsources.php
@@ -5,7 +5,7 @@ header("Content-Type: text/html; charset=".$CHARSET);
 
 $taxa = new Sources();
 
-$taxaArr = $taxa->getTaxaWithSources(10);
+$taxaArr = $taxa->getTaxaWithSources(20);
 $headerArr = ['Collection Category', 'NEON Taxon Type Code', 'collid', 'NEON Taxon ID', 'sciname', 'Source in Symbiota', 'Source in NEON API'];
 ?>
 <html>
--- misc/taxawithsourcescsv.php
+++ misc/taxawithsourcescsv.php
@@ -5,19 +5,17 @@ header("Content-Type: text/html; charset=".$CHARSET);
 
 $taxa = new Sources();
 
-$taxaArr = $taxa->getTaxaWithSources(20);
 $taxaCsv = $taxa->getTaxaWithSources(1000);
 $headerArr = ['Collection Category', 'NEON Taxon Type Code', 'collid', 'NEON Taxon ID', 'sciname', 'Source in Symbiota', 'Source in NEON API'];
 
-  if(!empty($taxaArr)){
+  if(!empty($taxaCsv)){
     $neonArr = array();
-    foreach ($taxaArr as &$row) {
+    foreach ($taxaCsv as &$row) {
       $sciname = $row['sciname'];
       $neonSource = $taxa->getNeonSourcesFromAPI($sciname);
       $row['neonsourcefromapi'] = $neonSource;
       // echo "$sciname - $neonSource <br>";
     };
-    $taxaTable = $taxa->htmlTable($taxaArr, $headerArr);
     $taxa->downloadTaxSources($taxaCsv,$headerArr,'biorepo-taxa-with-sources.csv');
     };
 ?>

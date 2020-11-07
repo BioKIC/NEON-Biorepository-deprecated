fd2a04b42723968d19cc805e45fab70ad1b723df
Andrew Johnston <ajohnston@asu.edu>
2020-11-06 17:23:47 -0700

    Update header.en.php

--- content/lang/header.en.php
+++ content/lang/header.en.php
@@ -9,7 +9,7 @@ $LANG['H_HOME'] = 'Home';
 
 $LANG['H_SEARCH'] = 'Search';
 $LANG['H_COLLECTIONS'] = 'Collections';
-$LANG['H_MAP'] = 'Map';
+$LANG['H_MAP'] = 'Map Search';
 $LANG['H_DYN_LISTS'] = 'Dynamic Species List';
 $LANG['H_TAXONOMIC_EXPLORER'] = 'Taxonomic Explorer';
 

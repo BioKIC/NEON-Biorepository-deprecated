6d9732590fc719d8e59b6e423ac1809cba833dbe
Laura Rocha Prado <lauraprado@asu.edu>
2021-01-14 15:05:47 -0700

    reordered css priorities

--- includes/head.php
+++ includes/head.php
@@ -17,14 +17,12 @@ if($activateJQuery){
 	echo '<link href="'.$CLIENT_ROOT.'/css/jquery-ui.css" type="text/css" rel="stylesheet">';
 }
 ?>
-<!-- FONT –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-<!-- <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700" rel="stylesheet" type="text/css">  -->
 <!-- UNIVERSAL CSS –––––––––––––––––––––––––––––––––––––––––––––––––– -->
 <link rel="stylesheet" href="<?php echo $CLIENT_ROOT; ?>/css/normalize.css">
 <link rel="stylesheet" href="<?php echo $CLIENT_ROOT; ?>/css/skeleton.css">
+<link rel="stylesheet" href="<?php echo $CLIENT_ROOT; ?>/css/base.css?ver=1" type="text/css">
 <link rel="stylesheet" href="<?php echo $CLIENT_ROOT; ?>/css/neon.css?ver=1">
-<link href="<?php echo $CLIENT_ROOT; ?>/css/base.css?ver=1" type="text/css" rel="stylesheet">
-<link href="<?php echo $CLIENT_ROOT; ?>/css/main.css?ver=1" type="text/css" rel="stylesheet">
+<link rel="stylesheet" href="<?php echo $CLIENT_ROOT; ?>/css/main.css?ver=1" type="text/css">
 
 <script type="text/javascript" src="<?php echo $CLIENT_ROOT; ?>/js/symb/base.js?ver=7"></script>
 <script type="text/javascript">

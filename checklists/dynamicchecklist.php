809cd1e4aea033ca6ddbee23675efb6bbaaf37be
Edward Gilbert <egbot@asu.edu>
2021-02-10 11:14:25 -0700

    Dynamic Checklist Generator dev
    - Fixed bug causing tool to fail that was introduced when adding variable sanitation code
    - Added code to ensure taxon filter being applied during various methods of applying a taxon terms
    - Extended Manager class into Dynamic Checklist class

--- checklists/dynamicchecklist.php
+++ checklists/dynamicchecklist.php
@@ -1,5 +1,4 @@
 <?php
-//error_reporting(E_ALL);
 include_once('../config/symbini.php');
 include_once($SERVER_ROOT.'/classes/DynamicChecklistManager.php');
 header("Content-Type: text/html; charset=".$CHARSET);
@@ -9,6 +8,7 @@ $lng = $_POST['lng'];
 $radius = $_POST['radius'];
 $radiusUnits = $_POST['radiusunits'];
 $dynamicRadius = (isset($DYN_CHECKLIST_RADIUS)?$DYN_CHECKLIST_RADIUS:10);
+$taxa = $_POST['taxa'];
 $tid = $_POST['tid'];
 $interface = $_POST['interface'];
 
@@ -18,24 +18,24 @@ if(!is_numeric($lng)) $lng = 0;
 if(!is_numeric($radius)) $radius = 0;
 if($radiusUnits != 'mi') $radiusUnits == 'km';
 if(!is_numeric($dynamicRadius)) $dynamicRadius = 10;
+$taxa = filter_var($taxa,FILTER_SANITIZE_STRING);
 if(!is_numeric($tid)) $tid = 0;
 
 $dynClManager = new DynamicChecklistManager();
 
-if(is_numeric($radius)){
-	$dynClid = $dynClManager->createChecklist($lat, $lng, $radius, $radiusUnits, $tid);
-}
-else{
-	$dynClid = $dynClManager->createDynamicChecklist($lat, $lng, $dynamicRadius, $tid);
-}
+if($taxa && !$tid) $tid = $dynClManager->getTid($taxa);
+$dynClid = 0;
+if($radius) $dynClid = $dynClManager->createChecklist($lat, $lng, $radius, $radiusUnits, $tid);
+else $dynClid = $dynClManager->createDynamicChecklist($lat, $lng, $dynamicRadius, $tid);
 
-if($interface == "key"){
-	header("Location: ".$CLIENT_ROOT."/ident/key.php?dynclid=".$dynClid."&taxon=All Species");
+if($dynClid){
+	if($interface == "key"){
+		header("Location: ".$CLIENT_ROOT."/ident/key.php?dynclid=".$dynClid."&taxon=All Species");
+	}
+	else{
+		header("Location: ".$CLIENT_ROOT."/checklists/checklist.php?dynclid=".$dynClid);
+	}
 }
-else{
-	header("Location: ".$CLIENT_ROOT."/checklists/checklist.php?dynclid=".$dynClid);
-}
-ob_flush();
-flush();
+else echo 'ERROR generating checklist';
 $dynClManager->removeOldChecklists();
 ?>

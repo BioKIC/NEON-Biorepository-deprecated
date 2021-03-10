e35518a4d7ef7c8b99d3ab7da88f5d98913eab99
Edward Gilbert <egbot@asu.edu>
2021-03-10 00:09:19 -0700

    Checklist Management dev
    - Added general inventory listing page to sitemap
    - Refactored and updated checklist index page
    - Added language support to checklist index page

--- checklists/index.php
+++ checklists/index.php
@@ -1,9 +1,10 @@
 <?php
 include_once('../config/symbini.php');
 include_once($SERVER_ROOT.'/classes/ChecklistManager.php');
+include_once($SERVER_ROOT.'/content/lang/checklists/index.'.$LANG_TAG.'.php');
 header("Content-Type: text/html; charset=".$CHARSET);
 
-$pid = array_key_exists("pid",$_REQUEST)?$_REQUEST["pid"]:0;
+$pid = array_key_exists('pid',$_REQUEST)?$_REQUEST['pid']:0;
 
 $clManager = new ChecklistManager();
 $clManager->setProj($pid);
@@ -21,57 +22,52 @@ $clManager->setProj($pid);
 		echo '<link href="'.$CLIENT_ROOT.'/css/base.css?ver=1" type="text/css" rel="stylesheet" />';
 		echo '<link href="'.$CLIENT_ROOT.'/css/main.css?ver=1" type="text/css" rel="stylesheet" />';
 	}
+	include_once($SERVER_ROOT.'/includes/googleanalytics.php');
 	?>
-	<script type="text/javascript">
-		<?php include_once($SERVER_ROOT.'/includes/googleanalytics.php'); ?>
-	</script>
 </head>
 <body>
 	<?php
-	$displayLeftMenu = (isset($checklists_indexMenu)?$checklists_indexMenu:"true");
-	include($SERVER_ROOT."/includes/header.php");
-	echo "<div class='navpath'>";
-	echo "<a href='../index.php'>Home</a> &gt;&gt; ";
-	if(isset($checklists_indexCrumbs) && $checklists_indexCrumbs) echo $checklists_indexCrumbs.' &gt;&gt;';
-	echo " <b>Species Checklists</b>";
-	echo "</div>";
+	$displayLeftMenu = (isset($checklists_indexMenu)?$checklists_indexMenu:'true');
+	include($SERVER_ROOT.'/includes/header.php');
 	?>
-	<!-- This is inner text! -->
+	<div class="navpath">
+		<a href="../index.php"><?php echo (isset($LANG['NAV_HOME'])?$LANG['NAV_HOME']:'Home'); ?></a> &gt;&gt;
+		<b><?php echo (isset($LANG['SPECIES_INVENTORIES'])?$LANG['SPECIES_INVENTORIES']:'Species Inventories'); ?></b>
+	</div>
 	<div id="innertext">
-		<h1>Species Checklists</h1>
-        <div style='margin:20px;'>
+		<h1><?php echo (isset($LANG['SPECIES_INVENTORIES'])?$LANG['SPECIES_INVENTORIES']:'Species Inventories'); ?></h1>
+        <div style="margin:20px;">
 			<?php
             $researchArr = $clManager->getChecklists();
 			if($researchArr){
 				foreach($researchArr as $pid => $projArr){
 					?>
-					<div style='margin:3px 0px 0px 15px;'>
-						<h3><?php echo $projArr['name']; ?>
-							<a href="<?php echo "clgmap.php?pid=".$pid; ?>" title='Show checklists on map'>
-								<img src='../images/world.png' style='width:10px;border:0' />
-							</a>
-						</h3>
-						<div>
-							<ul>
-								<?php
-								foreach($projArr['clid'] as $clid => $clName){
-									echo "<li><a href='checklist.php?clid=".$clid."&pid=".$pid."'>".$clName."</a></li>\n";
-								}
-								?>
-							</ul>
-						</div>
-					</div>
+					<h3>
+						<?php
+						$projName = $projArr['name'];
+						if($projName == 'Miscellaneous Inventories') $projName = (isset($LANG['MISC_INVENTORIES'])?$LANG['MISC_INVENTORIES']:'Miscellaneous Inventories');
+						echo $projName;
+						?>
+						<a href="<?php echo "clgmap.php?pid=".$pid; ?>" title='<?php echo (isset($LANG['SHOW_MAP'])?$LANG['SHOW_MAP']:'Show inventories on map'); ?>'>
+							<img src='../images/world.png' style='width:10px;border:0' />
+						</a>
+					</h3>
+					<ul>
+						<?php
+						foreach($projArr['clid'] as $clid => $clName){
+							echo '<li><a href="checklist.php?clid='.$clid.'&pid='.$pid.'">'.$clName.'</a></li>';
+						}
+						?>
+					</ul>
 					<?php
 				}
 			}
-			else{
-				echo '<div><b>No Checklists returned</b></div>';
-			}
+			else echo '<div><b>'.(isset($LANG['NO_INVENTORIES'])?$LANG['NO_INVENTORIES']:'No inventories returned').'</b></div>';
 			?>
 		</div>
 	</div>
 	<?php
-		include($SERVER_ROOT."/includes/footer.php");
+	include($SERVER_ROOT.'/includes/footer.php');
 	?>
 </body>
 </html>

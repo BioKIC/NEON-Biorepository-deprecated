37001b3fd008532ff25f9d39e98c2d50c9741f02
Edward Gilbert <egbot@asu.edu>
2021-03-26 08:33:00 -0400

    Update harvestparams.es.php

--- content/lang/collections/harvestparams.es.php
+++ content/lang/collections/harvestparams.es.php
@@ -56,6 +56,7 @@ $LANG['INCLUDE_OTHER_CATNUM'] = 'Includa todos n&uacute;meros de cat&eacute;logo
 $LANG['TYPE'] = 'Limitar a ejemplares tipo';
 $LANG['HAS_IMAGE'] = 'Limitar a ejemplares con im&aacute;genes';
 $LANG['HAS_GENETIC'] = 'Limitar a ejemplares con datos gen&eacute;ticos';
+$LANG['HAS_COORDS'] = 'Limitar a ejemplares con coordenadas geogr&aacute;ficas';
 $LANG['INCLUDE_CULTIVATED'] = 'Incluye ejemplares cultivadas/cautivas';
 $LANG['TRAIT_HEADER'] = 'Criterios de Rasgos';
 ?>

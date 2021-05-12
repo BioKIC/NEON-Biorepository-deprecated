49e76968efd9ed02884e83e5aaba39fe726b3074
Edward Gilbert <egbot@asu.edu>
2021-05-12 16:36:44 -0400

    Head include modification
    - Make jQuery-UI CSS available to all pages

--- includes/head.php
+++ includes/head.php
@@ -11,11 +11,6 @@
 //$CUSTOM_CSS_PATH = '/css/symb/custom';
 ?>
 <meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />
-<?php
-if($activateJQuery){
-	//For an alternate jQuery UI styling, point link below to another css file
-	echo '<link href="'.$CLIENT_ROOT.'/css/jquery-ui.css" type="text/css" rel="stylesheet">';
-}
-?>
+<link href="<?php echo $CLIENT_ROOT; ?>/css/jquery-ui.css" type="text/css" rel="stylesheet">
 <link href="<?php echo $CLIENT_ROOT; ?>/css/base.css?ver=1" type="text/css" rel="stylesheet">
 <link href="<?php echo $CLIENT_ROOT; ?>/css/main.css?ver=1" type="text/css" rel="stylesheet">

13aca66b8b85057ac6a9f33c9a94daa4d0b63f5a
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-06-18 07:54:19 -0700

    Language tags for specimenstoid.php

--- profile/specimenstoid.php
+++ profile/specimenstoid.php
@@ -1,6 +1,7 @@
 <?php
 include_once('../config/symbini.php');
 include_once($SERVER_ROOT.'/classes/ProfileManager.php');
+include_once($SERVER_ROOT.'/content/lang/profile/specimenstoid.'.$LANG_TAG.'.php');
 header("Content-Type: text/html; charset=".$CHARSET);
 
 $userId = $_REQUEST['userid'];
@@ -29,10 +30,10 @@ $profileHandler->setUid($SYMB_UID);
 		<?php 
 		if($userId){
 			if($action == 'showmissingids'){
-				echo '<a href="viewprofile.php?tabindex=2&userid='.$userId.'"><b>Display Specimens within your Taxonomic Scope</b></a>';
+				echo '<a href="viewprofile.php?tabindex=2&userid='.$userId.'"><b>'.(isset($LANG['DISPLAY_SPECS'])?$LANG['DISPLAY_SPECS']:'Display Specimens within your Taxonomic Scope').'</b></a>';
 			}
 			else{
-				echo '<a href="viewprofile.php?action=showmissingids&tabindex=2&userid='.$userId.'"><b>Order Level or Above (open to all identification editors)</b></a>';
+				echo '<a href="viewprofile.php?action=showmissingids&tabindex=2&userid='.$userId.'"><b>'.(isset($LANG['ORDER'])?$LANG['ORDER']:'Order Level or Above (open to all identification editors)').'</b></a>';
 			}
 		}
 		?>

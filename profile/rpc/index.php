71dcb3dc5b6ec5888e27834b7c0199daeadaa815
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-06-21 14:01:25 -0700

    Language tags for profile/rpc/index.php

--- profile/rpc/index.php
+++ profile/rpc/index.php
@@ -1,12 +1,13 @@
 <?php
 //error_reporting(E_ALL);
  include_once('../../config/symbini.php');
+ include_once($SERVER_ROOT.'/content/lang/profile/rpc/index.'.$LANG_TAG.'.php');
  header("Content-Type: text/html; charset=".$CHARSET);
  
 ?>
 <html>
 	<head>
-		<title>Page</title>
+		<title><?php echo (isset($LANG['PAGE'])?$LANG['PAGE']:'Page'); ?></title>
     <?php
       $activateJQuery = false;
       if(file_exists($SERVER_ROOT.'/includes/head.php')){
@@ -26,12 +27,12 @@
 		?>
 		<!-- This is inner text! -->
 		<div id="innertext">
-			<h1>Forbidden</h1>
+			<h1><?php echo (isset($LANG['FORBIDDEN'])?$LANG['FORBIDDEN']:'Forbidden'); ?></h1>
 			<div style="font-weight:bold;">
-				You don't have permission to access this page.
+				<?php echo (isset($LANG['NO_PERMISSION'])?$LANG['NO_PERMISSION']:'You don\'t have permission to access this page'); ?>.
 			</div>
 			<div style="font-weight:bold;margin:10px;">
-				<a href="<?php echo $CLIENT_ROOT; ?>/index.php">Return to index page</a>
+				<a href="<?php echo $CLIENT_ROOT; ?>/index.php"><?php echo (isset($LANG['RETURN'])?$LANG['RETURN']:'Return to index page'); ?></a>
 			</div>
 		</div>
 		<?php

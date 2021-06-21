0050569c68b49b1f1ef6d76d448ea23846e16d62
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-06-21 11:45:45 -0700

    Language tags for userprofile.php

--- profile/userprofile.php
+++ profile/userprofile.php
@@ -1,6 +1,7 @@
 <?php
 include_once('../config/symbini.php');
 include_once($SERVER_ROOT.'/classes/ProfileManager.php');
+include_once($SERVER_ROOT.'/content/lang/profile/userprofile.'.$LANG_TAG.'.php');
 header("Content-Type: text/html; charset=".$CHARSET);
 
 $userId = $_REQUEST["userid"];
@@ -18,7 +19,7 @@ if($userId != $SYMB_UID) $isSelf = false;
 <div style="padding:15px;">
 	<div>
 		<div>
-			<b><u>Profile Details</u></b>
+			<b><u><?php echo (isset($LANG['DETAILS'])?$LANG['DETAILS']:'Profile Details'); ?></u></b>
 		</div>
 		<div style="margin:20px;">
 			<?php
@@ -44,20 +45,20 @@ if($userId != $SYMB_UID) $isSelf = false;
 			echo '<div>User information: '.($person->getIsPublic()?'public':'private').'</div>';
 			?>
 			<div style="font-weight:bold;margin-top:10px;">
-				<div><a href="#" onclick="toggleEditingTools('profileeditdiv');return false;">Edit Profile</a></div>
-				<div><a href="#" onclick="toggleEditingTools('pwdeditdiv');return false;">Change Password</a></div>
-				<div><a href="#" onclick="toggleEditingTools('logineditdiv');return false;">Change Login</a></div>
-                <div><a href="#" onclick="toggleEditingTools('managetokensdiv');return false;">Manage Access</a></div>
+				<div><a href="#" onclick="toggleEditingTools('profileeditdiv');return false;"><?php echo (isset($LANG['EDIT_PROFILE'])?$LANG['EDIT_PROFILE']:'Edit Profile'); ?></a></div>
+				<div><a href="#" onclick="toggleEditingTools('pwdeditdiv');return false;"><?php echo (isset($LANG['CHANGE_PASSWORD'])?$LANG['CHANGE_PASSWORD']:'Change Password'); ?></a></div>
+				<div><a href="#" onclick="toggleEditingTools('logineditdiv');return false;"><?php echo (isset($LANG['CHANGE_LOGIN'])?$LANG['CHANGE_LOGIN']:'Change Login'); ?></a></div>
+                <div><a href="#" onclick="toggleEditingTools('managetokensdiv');return false;"><?php echo (isset($LANG['MANAGE_ACCESS'])?$LANG['MANAGE_ACCESS']:'Manage Access'); ?></a></div>
 			</div>
 		</div>
 	</div>
 	<div id="profileeditdiv" style="display:none;margin:15px;">
 		<form name="editprofileform" action="viewprofile.php" method="post">
 			<fieldset>
-				<legend><b>Edit User Profile</b></legend>
+				<legend><b><?php echo (isset($LANG['EDIT_U_PROFILE'])?$LANG['EDIT_U_PROFILE']:'Edit User Profile'); ?></b></legend>
 				<table cellspacing='1' style="width:100%;">
 				    <tr>
-				        <td><b>First Name:</b></td>
+				        <td><b><?php echo (isset($LANG['FIRST_NAME'])?$LANG['FIRST_NAME']:'First Name'); ?>:</b></td>
 				        <td>
 							<div>
 								<input id="firstname" name="firstname" size="40" value="<?php echo $person->getFirstName();?>" required />
@@ -65,7 +66,7 @@ if($userId != $SYMB_UID) $isSelf = false;
 			            </td>
 				    </tr>
 				    <tr>
-				        <td><b>Last Name:</b></td>
+				        <td><b><?php echo (isset($LANG['LAST_NAME'])?$LANG['LAST_NAME']:'Last Name'); ?>:</b></td>
 				        <td>
 							<div>
 								<input id="lastname" name="lastname" size="40" value="<?php echo $person->getLastName();?>" required />
@@ -73,7 +74,7 @@ if($userId != $SYMB_UID) $isSelf = false;
 			            </td>
 				    </tr>
 				    <tr>
-				        <td><b>Email Address:</b></td>
+				        <td><b><?php echo (isset($LANG['EMAIL'])?$LANG['EMAIL']:'Email Address'); ?>:</b></td>
 				        <td>
 							<div>
 					            <input id="email" name="email" type="email" size="40" value="<?php echo $person->getEmail();?>" required />
@@ -81,7 +82,7 @@ if($userId != $SYMB_UID) $isSelf = false;
 			            </td>
 				    </tr>
 				    <tr>
-				        <td><b>ORCID or other GUID:</b></td>
+				        <td><b><?php echo (isset($LANG['ORCID'])?$LANG['ORCID']:'ORCID or other GUID'); ?>:</b></td>
 				        <td>
 							<div>
 					            <input name="guid" type="text" size="40" value="<?php echo $person->getGUID();?>" />
@@ -89,7 +90,7 @@ if($userId != $SYMB_UID) $isSelf = false;
 			            </td>
 				    </tr>
 				    <tr>
-				        <td><b>Title:</b></td>
+				        <td><b><?php echo (isset($LANG['TITLE'])?$LANG['TITLE']:'Title'); ?>:</b></td>
 				        <td>
 							<div>
 								<input name="title"  size="40" value="<?php echo $person->getTitle();?>">
@@ -97,7 +98,7 @@ if($userId != $SYMB_UID) $isSelf = false;
 						</td>
 				    </tr>
 				    <tr>
-				        <td><b>Institution:</b></td>
+				        <td><b><?php echo (isset($LANG['INSTITUTION'])?$LANG['INSTITUTION']:'Institution'); ?>:</b></td>
 				        <td>
 							<div>
 								<input name="institution"  size="40" value="<?php echo $person->getInstitution();?>">
@@ -105,7 +106,7 @@ if($userId != $SYMB_UID) $isSelf = false;
 						</td>
 				    </tr>
 				    <tr>
-				        <td><b>City:</b></td>
+				        <td><b><?php echo (isset($LANG['CITY'])?$LANG['CITY']:'City'); ?>:</b></td>
 				        <td>
 							<div>
 				            	<input id="city" name="city" size="40" value="<?php echo $person->getCity();?>">
@@ -113,7 +114,7 @@ if($userId != $SYMB_UID) $isSelf = false;
 			            </td>
 				    </tr>
 				    <tr>
-				        <td><b>State:</b></td>
+				        <td><b><?php echo (isset($LANG['STATE'])?$LANG['STATE']:'State'); ?>:</b></td>
 				        <td>
 							<div>
 					            <input id="state" name="state" size="40" value="<?php echo $person->getState();?>">
@@ -121,7 +122,7 @@ if($userId != $SYMB_UID) $isSelf = false;
 			            </td>
 				    </tr>
 				    <tr>
-				        <td><b>Zip Code:</b></td>
+				        <td><b><?php echo (isset($LANG['ZIP'])?$LANG['ZIP']:'Zip Code'); ?>:</b></td>
 				        <td>
 							<div>
 					            <input name="zip" size="40" value="<?php echo $person->getZip();?>">
@@ -129,7 +130,7 @@ if($userId != $SYMB_UID) $isSelf = false;
 			            </td>
 				    </tr>
 				    <tr>
-				        <td><b>Country:</b></td>
+				        <td><b><?php echo (isset($LANG['COUNTRY'])?$LANG['COUNTRY']:'Country'); ?>:</b></td>
 				        <td>
 							<div>
 								<input id="country" name="country" size="40" value="<?php echo $person->getCountry();?>">
@@ -137,7 +138,7 @@ if($userId != $SYMB_UID) $isSelf = false;
 						</td>
 				    </tr>
 				    <tr>
-				        <td><b>Url:</b></td>
+				        <td><b><?php echo (isset($LANG['URL'])?$LANG['URL']:'URL'); ?>:</b></td>
 				        <td>
 							<div>
 								<input name="url"  size="40" value="<?php echo $person->getUrl();?>">
@@ -146,7 +147,7 @@ if($userId != $SYMB_UID) $isSelf = false;
 						</td>
 				    </tr>
 				    <tr>
-				        <td><b>Biography:</b></td>
+				        <td><b><?php echo (isset($LANG['BIOGRAPHY'])?$LANG['BIOGRAPHY']:'Biography'); ?>:</b></td>
 				        <td>
 							<div>
 								<textarea name="biography" rows="4" cols="40"><?php echo $person->getBiography();?></textarea>
@@ -157,7 +158,7 @@ if($userId != $SYMB_UID) $isSelf = false;
 				        <td colspan="2">
 							<div>
 								<input type="checkbox" name="ispublic" value="1" <?php if($person->getIsPublic()) echo "CHECKED"; ?> />
-								Make user information displayable to public
+								<?php echo (isset($LANG['MAKE_PUBLIC'])?$LANG['MAKE_PUBLIC']:'Make user information displayable to public'); ?>
 			        		</div>
 						</td>
 				    </tr>
@@ -165,32 +166,32 @@ if($userId != $SYMB_UID) $isSelf = false;
 						<td colspan="2">
 							<div style="margin:10px;">
 								<input type="hidden" name="userid" value="<?php echo $userId;?>" />
-								<input type="submit" name="action" value="Submit Edits" />
+								<input type="submit" name="action" value="<?php echo (isset($LANG['SUBMIT_EDITS'])?$LANG['SUBMIT_EDITS']:'Submit Edits'); ?>" />
 							</div>
 						</td>
 					</tr>
 				</table>
 			</fieldset>
 		</form>
-		<form name="delprofileform" action="viewprofile.php" method="post" onsubmit="return window.confirm('Are you sure you want to delete profile?');">
+		<form name="delprofileform" action="viewprofile.php" method="post" onsubmit="return window.confirm('<?php echo (isset($LANG['SURE_DELETE'])?$LANG['SURE_DELETE']:'Are you sure you want to delete profile?'); ?>');">
 			<fieldset style='padding:15px;width:200px;'>
-		    	<legend><b>Delete Profile</b></legend>
+		    	<legend><b><?php echo (isset($LANG['DELETE_PROF'])?$LANG['DELETE_PROF']:'Delete Profile'); ?></b></legend>
 				<input type="hidden" name="userid" value="<?php echo $userId;?>" />
-	    		<input type="submit" name="action" value="Delete Profile" />
+	    		<input type="submit" name="action" value="<?php echo (isset($LANG['DELETE_PROF'])?$LANG['DELETE_PROF']:'Delete Profile'); ?>" />
 			</fieldset>
 		</form>
 	</div>
 	<div id="pwdeditdiv" style="display:none;margin:15px;">
 		<form name="changepwdform" action="viewprofile.php" method="post" onsubmit="return verifyPwdForm(this);">
 			<fieldset style='padding:15px;width:500px;'>
-		    	<legend><b>Change Password</b></legend>
+		    	<legend><b><?php echo (isset($LANG['CHANGE_PASSWORD'])?$LANG['CHANGE_PASSWORD']:'Change Password'); ?></b></legend>
 		    	<table>
 					<?php
 					if($isSelf){
 						?>
 			    		<tr>
 			    			<td>
-				            	<b>Current Password:</b>
+				            	<b><?php echo (isset($LANG['CURRENT_PWORD'])?$LANG['CURRENT_PWORD']:'Current Password'); ?>:</b>
 				            </td>
 				            <td>
 				            	<input id="oldpwd" name="oldpwd" type="password"/>
@@ -201,7 +202,7 @@ if($userId != $SYMB_UID) $isSelf = false;
 					?>
 		    		<tr>
 		    			<td>
-			            	<b>New Password:</b>
+			            	<b><?php echo (isset($LANG['NEW_PWORD'])?$LANG['NEW_PWORD']:'New Password'); ?>:</b>
 			            </td>
 			            <td>
 			            	<input id="newpwd" name="newpwd" type="password"/>
@@ -209,7 +210,7 @@ if($userId != $SYMB_UID) $isSelf = false;
 		    		</tr>
 		    		<tr>
 		    			<td>
-							<b>New Password Again:</b>
+							<b><?php echo (isset($LANG['PWORD_AGAIN'])?$LANG['PWORD_AGAIN']:'New Password Again'); ?>:</b>
 			            </td>
 			            <td>
 							<input id="newpwd2" name="newpwd2" type="password"/>
@@ -218,7 +219,7 @@ if($userId != $SYMB_UID) $isSelf = false;
 		    		<tr>
 		    			<td colspan="2">
 							<input type="hidden" name="userid" value="<?php echo $userId;?>" />
-							<input type="submit" name="action" value="Change Password" />
+							<input type="submit" name="action" value="<?php echo (isset($LANG['CHANGE_PASSWORD'])?$LANG['CHANGE_PASSWORD']:'Change Password'); ?>" />
 		    			</td>
 		    		</tr>
 				</table>
@@ -227,78 +228,78 @@ if($userId != $SYMB_UID) $isSelf = false;
 	</div>
 	<div id="logineditdiv" style="display:none;margin:15px;">
 		<fieldset style='padding:15px;width:500px;'>
-	    	<legend><b>Change Login Name</b></legend>
+	    	<legend><b><?php echo (isset($LANG['CHANGE_USERNAME'])?$LANG['CHANGE_USERNAME']:'Change Username'); ?></b></legend>
 			<form name="modifyloginform" action="viewprofile.php" method="post" onsubmit="return verifyModifyLoginForm(this);">
-				<div><b>New Login Name:</b> <input name="newlogin" type="text" /></div>
+				<div><b><?php echo (isset($LANG['NEW_USERNAME'])?$LANG['NEW_USERNAME']:'New Username'); ?>:</b> <input name="newlogin" type="text" /></div>
 				<?php
 				if($isSelf){
 					?>
-					<div><b>Current Password:</b> <input name="newloginpwd" id="newloginpwd" type="password" /></div>
+					<div><b><?php echo (isset($LANG['CURRENT_PWORD'])?$LANG['CURRENT_PWORD']:'Current Password'); ?>:</b> <input name="newloginpwd" id="newloginpwd" type="password" /></div>
 					<?php
 				}
 				?>
 				<div style="margin:10px">
 					<input type="hidden" name="userid" value="<?php echo $userId;?>" />
-					<input type="submit" name="action" value="Change Login" />
+					<input type="submit" name="action" value="<?php echo (isset($LANG['CHANGE_USERNAME'])?$LANG['CHANGE_USERNAME']:'Change Username'); ?>" />
 				</div>
 			</form>
 		</fieldset>
 	</div>
     <div id="managetokensdiv" style="display:none;margin:15px;">
         <fieldset style='padding:15px;width:500px;'>
-            <legend><b>Manage Access Tokens</b></legend>
+            <legend><b><?php echo (isset($LANG['MANAGE_TOKENS'])?$LANG['MANAGE_TOKENS']:'Manage Access Tokens'); ?></b></legend>
             <form name="cleartokenform" action="viewprofile.php" method="post" onsubmit="">
-                <div>You currently have <b><?php echo ($tokenCount?$tokenCount:0); ?></b> access tokens linked to your account.
-                    Tokens are created when you select "Remember Me" when logging in, or access the portal
-                    from an external app. If the number of access tokens you have seems high, please click on the button below to
-                    clear the tokens linked to your account.</div>
+                <div><?php echo (isset($LANG['YOU_HAVE'])?$LANG['YOU_HAVE']:'You currently have').' <b>'.($tokenCount?$tokenCount:0).' </b>'.
+				(isset($LANG['EXPLAIN_TOKENS'])?$LANG['EXPLAIN_TOKENS']:'access tokens linked to your account. Tokens are created when you select "Remember Me" 
+				when logging in, or access the portal from an external app. If the number of access tokens you have seems high, please click on the button below 
+				to clear the tokens linked to your account.'); ?>
+                </div>
                 <div style="margin:10px">
                     <input type="hidden" name="userid" value="<?php echo $userId;?>" />
-                    <input type="submit" name="action" value="Clear Tokens" />
+                    <input type="submit" name="action" value="<?php echo (isset($LANG['CLEAR_TOKENS'])?$LANG['CLEAR_TOKENS']:'Clear Tokens'); ?>" />
                 </div>
             </form>
         </fieldset>
     </div>
 	<div>
 		<div>
-			<b><u>Taxonomic Relationships</u></b>
-			<a href="#" onclick="toggle('addtaxonrelationdiv')" title="Add a new taxonomic relationship">
+			<b><u><?php echo (isset($LANG['TAXON_RELS'])?$LANG['TAXON_RELS']:'Taxonomic Relationships'); ?></u></b>
+			<a href="#" onclick="toggle('addtaxonrelationdiv')" title="<?php echo (isset($LANG['ADD_TAXON_REL'])?$LANG['ADD_TAXON_REL']:'Add a New Taxonomic Relationship'); ?>">
 				<img style='border:0px;width:15px;' src='../images/add.png'/>
 			</a>
 		</div>
 		<div id="addtaxonrelationdiv" style="display:none;">
 			<fieldset style="padding:20px;margin:15px;">
-				<legend><b>New Taxonomic Region of Interest</b></legend>
+				<legend><b><?php echo (isset($LANG['NEW_TAX_REGION'])?$LANG['NEW_TAX_REGION']:'New Taxonomic Region of Interest'); ?></b></legend>
 				<div style="margin-bottom:10px;">
-					Use this form to define a new taxon-based region of interest.
-					Contact portal administrators for assignment of new
-					taxon specific Occurrence Identification and Taxonomic Thesaurus editing status.
+					<?php echo (isset($LANG['TAX_FORM'])?$LANG['TAX_FORM']:'Use this form to define a new taxon-based region of interest. Contact portal 
+					administrators for assignment of new taxon specific Occurrence Identification and Taxonomic Thesaurus editing status.'); ?>
 				</div>
 				<form name="addtaxonomyform" action="viewprofile.php" method="post" onsubmit="return verifyAddTaxonomyForm(this)">
 					<div style="margin:3px;">
-						<b>Taxon</b><br/>
+						<b><?php echo (isset($LANG['TAXON'])?$LANG['TAXON']:'Taxon'); ?></b><br/>
 						<input id="taxoninput" name="taxon" type="text" value="" style="width:90%;" onfocus="initTaxonAutoComplete()" />
 					</div>
 					<div style="margin:3px;">
-						<b>Scope of Relationship</b><br/>
+						<b><?php echo (isset($LANG['SCOPE_OF_REL'])?$LANG['SCOPE_OF_REL']:'Scope of Relationship'); ?></b><br/>
 						<select name="editorstatus">
-							<option value="RegionOfInterest">Region Of Interest</option>
+							<option value="RegionOfInterest"><?php echo (isset($LANG['REGION'])?$LANG['REGION']:'Region of Interest'); ?></option>
 							<!-- <option value="OccurrenceEditor">Occurrence Editor</option> -->
 						</select>
 
 					</div>
 					<div style="margin:3px;">
-						<b>Geographic Scope Limits</b><br/>
+						<b><?php echo (isset($LANG['SCOPE_LIMITS'])?$LANG['SCOPE_LIMITS']:'Geographic Scope Limits'); ?></b><br/>
 						<input name="geographicscope" type="text" value="" style="width:90%;"/>
 
 					</div>
 					<div style="margin:3px;">
-						<b>Notes</b><br/>
+						<b><?php echo (isset($LANG['NOTES'])?$LANG['NOTES']:'Notes'); ?></b><br/>
 						<input name="notes" type="text" value="" style="width:90%;" />
 
 					</div>
 					<div style="margin:20px 10px;">
-						<input name="action" type="submit" value="Add Taxonomic Relationship" />
+						<input name="action" type="submit" value="<?php echo (isset($LANG['ADD_TAX'])?$LANG['ADD_TAX']:'Add Taxonomic Relationship'); ?>" />
 					</div>
 				</form>
 			</fieldset>
@@ -308,9 +309,9 @@ if($userId != $SYMB_UID) $isSelf = false;
 		if($userTaxonomy){
 			ksort($userTaxonomy);
 			foreach($userTaxonomy as $cat => $userTaxArr){
-				if($cat == 'RegionOfInterest') $cat = 'Region Of Interest';
-				elseif($cat == 'OccurrenceEditor') $cat = 'Occurrence Editor';
-				elseif($cat == 'TaxonomicThesaurusEditor') $cat = 'Taxonomic Thesaurus Editor';
+				if($cat == 'RegionOfInterest') $cat = (isset($LANG['REGION'])?$LANG['REGION']:'Region Of Interest');
+				elseif($cat == 'OccurrenceEditor') $cat = (isset($LANG['OCC_EDIT'])?$LANG['OCC_EDIT']:'Occurrence Editor');
+				elseif($cat == 'TaxonomicThesaurusEditor') $cat = (isset($LANG['TAX_THES'])?$LANG['TAX_THES']:'Taxonomic Thesaurus Editor');
 				echo '<div style="margin:10px;">';
 				echo '<div><b>'.$cat.'</b></div>';
 				echo '<ul style="margin:10px;">';
@@ -327,7 +328,7 @@ if($userId != $SYMB_UID) $isSelf = false;
 			}
 		}
 		else{
-			echo '<div style="margin:20px;">No relationships defined</div>';
+			echo '<div style="margin:20px;">'.(isset($LANG['NO_RELS'])?$LANG['NO_RELS']:'No relationships defined').'</div>';
 		}
 		?>
 	</div>

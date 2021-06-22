f26206e93cfacc0b712d7a0b9fcee55d6d2fa7b1
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-06-22 15:06:06 -0700

    Language tags for dynamicmap and vaconflicts

--- checklists/vaconflicts.php
+++ checklists/vaconflicts.php
@@ -1,6 +1,7 @@
 <?php
 include_once('../config/symbini.php');
 include_once($SERVER_ROOT.'/classes/ChecklistVoucherReport.php');
+include_once($SERVER_ROOT.'/content/lang/checklists/vaconflicts.'.$LANG_TAG.'.php');
 
 $action = array_key_exists("submitaction",$_REQUEST)?$_REQUEST["submitaction"]:"";
 $clid = array_key_exists("clid",$_REQUEST)?$_REQUEST["clid"]:0;
@@ -35,18 +36,19 @@ if($IS_ADMIN || (array_key_exists("ClAdmin",$USER_RIGHTS) && in_array($clid,$USE
 			}
 		}
 		if(!formVerified){
-			alert("At least one voucher record needs to be selected");
+			alert("<?php echo (isset($LANG['SELECT_ONE'])?$LANG['SELECT_ONE']:'At least one voucher record needs to be selected'); ?>");
 			return false;
 		}
 		f.submit();
 	}
 </script>
 <div id="innertext" style="background-color:white;">
-	<h2>Possible Voucher Conflicts</h2>
+	<h2><?php echo (isset($LANG['POSS_CONFLICTS'])?$LANG['POSS_CONFLICTS']:'Possible Voucher Conflicts'); ?></h2>
 	<div style="margin-bottom:10px;">
-		List of specimen vouchers where the current identifications conflict with the checklist.
-		Voucher conflicts are typically due to recent annotations of specimens located within collection.
-		Click on Checklist ID to open the editing pane for that record.
+		<?php echo (isset($LANG['POSS_CONFLICTS'])?$LANG['POSS_CONFLICTS']:'List of specimen vouchers where the current identifications conflict with the checklist. 
+		Voucher conflicts are typically due to recent annotations of specimens located within collection. 
+		Click on Checklist ID to open the editing pane for that record.');
+		?>
 	</div>
 	<?php
 	if($conflictArr = $vManager->getConflictVouchers()){
@@ -56,10 +58,10 @@ if($IS_ADMIN || (array_key_exists("ClAdmin",$USER_RIGHTS) && in_array($clid,$USE
 			<table class="styledtable" style="font-family:Arial;font-size:12px;">
 				<tr>
 					<th><input type="checkbox" onclick="selectAll(this)" /></th>
-					<th><b>Checklist ID</b></th>
-					<th><b>Voucher Specimen</b></th>
-					<th><b>Corrected Specimen ID</b></th>
-					<th><b>Identified By</b></th>
+					<th><b><?php echo (isset($LANG['CHECK_ID'])?$LANG['CHECK_ID']:'Checklist ID'); ?></b></th>
+					<th><b><?php echo (isset($LANG['VOUCHER_SPEC'])?$LANG['VOUCHER_SPEC']:'Voucher Specimen'); ?></b></th>
+					<th><b><?php echo (isset($LANG['CORRECTED_ID'])?$LANG['CORRECTED_ID']:'Corrected Specimen ID'); ?></b></th>
+					<th><b><?php echo (isset($LANG['IDED_BY'])?$LANG['IDED_BY']:'Identified By'); ?></b></th>
 				</tr>
 				<?php
 				foreach($conflictArr as $id => $vArr){
@@ -75,7 +77,7 @@ if($IS_ADMIN || (array_key_exists("ClAdmin",$USER_RIGHTS) && in_array($clid,$USE
 								?>
 							</a>
 							<?php
-							if($vArr['clid'] != $clid) echo '<br/>(from child checklists)';
+							if($vArr['clid'] != $clid) echo '<br/>'.(isset($LANG['FROM_CHILD'])?$LANG['FROM_CHILD']:'(from child checklists)');
 							?>
 						</td>
 						<td>
@@ -95,21 +97,21 @@ if($IS_ADMIN || (array_key_exists("ClAdmin",$USER_RIGHTS) && in_array($clid,$USE
 				?>
 			</table>
 			<div>
-				<input name="removetaxa" type="checkbox" value="1" checked /> Remove taxa from checklist if all vouchers are removed
+				<input name="removetaxa" type="checkbox" value="1" checked /> <?php echo (isset($LANG['REMOVE_TAXA'])?$LANG['REMOVE_TAXA']:'Remove taxa from checklist if all vouchers are removed'); ?>
 			</div>
 			<div style="margin: 10px 0px">
 				<input name="clid" type="hidden" value="<?php echo $clid; ?>" />
 				<input name="pid" type="hidden" value="<?php echo $pid; ?>" />
 				<input name="tabindex" type="hidden" value="2" />
 				<input name="submitaction" type="hidden" value="resolveconflicts" />
-				<b>Batch Action:</b> <input name="submitbutton" type="button" value="Link Vouchers to Corrected Identification" onclick="return validateBatchConflictForm(this.form)" /><br/>
-				<div>* Corrected taxon will be added to checklist if not yet present</div>
+				<b><?php echo (isset($LANG['BATCH_ACTION'])?$LANG['BATCH_ACTION']:'Batch Action'); ?>:</b> <input name="submitbutton" type="button" value="<?php echo (isset($LANG['LINK_VOUCHERS'])?$LANG['LINK_VOUCHERS']:'Link Vouchers to Corrected Identification'); ?>" onclick="return validateBatchConflictForm(this.form)" /><br/>
+				<div>* <?php echo (isset($LANG['CORRECTED_WILL_ADD'])?$LANG['CORRECTED_WILL_ADD']:'Corrected taxon will be added to checklist if not yet present'); ?></div>
 			</div>
 		</form>
 		<?php
 	}
 	else{
-		echo '<h3>No conflicts exist</h3>';
+		echo '<h3>'.(isset($LANG['NO_CONFLICTS'])?$LANG['NO_CONFLICTS']:'No conflicts exist').'</h3>';
 	}
 	?>
 </div>

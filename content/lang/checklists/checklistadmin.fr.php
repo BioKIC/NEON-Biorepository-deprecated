4862ee92bd22c0833821b4ca6b38314ebcfbe387
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-06-24 14:26:11 -0700

    French checklist tags and edits to English checklist menu

---
+++ content/lang/checklists/checklistadmin.fr.php
@@ -0,0 +1,72 @@
+<?php
+/*
+------------------
+Language: French
+------------------
+*/
+
+include_once('checklist.fr.php');
+
+$LANG['SELECTPROJECT'] = 'S&eacute;lectionner un Projet';
+$LANG['RETURNCHECK'] = 'Retour &agrave; Liste';
+$LANG['ADMIN'] = 'Admin';
+$LANG['DESCRIPTION'] = 'Description';
+$LANG['RELATEDCHECK'] = 'Listes Connexes';
+$LANG['ADDIMGVOUCHER'] = 'Ajouter Image d\'&Eacute;chantillon';
+$LANG['NOEDITOR'] = 'Personne n\'a &eacute;t&eacute; explicitement attribu&eacute; comme &eacute;diteur';
+$LANG['ADDNEWUSER'] = 'Ajouter Nouvel Utilisateur';
+$LANG['SELECTUSER'] = 'S&acute;lectionner Utilisateur';
+$LANG['INVENTORYPROJECTS'] = 'Affectations de Projets d\'Inventaire';
+$LANG['REMOVEPROJECTCONFIRM'] = '&Ecirc;tes-vous s&ucirc;r de vouloir dissocier le projet?';
+$LANG['REMOVEPROJECT'] = 'Dissocier Liste du Projet';
+$LANG['CHECKNOTASSIGNED'] = 'Liste n\'a &eacute;t&eacute; attribu&eacute;e &agrave; aucun projet d\'inventaire';
+$LANG['LINKTOPROJECT'] = 'Ajouter &agrave; un Projet';
+$LANG['SUBMIT'] = 'Soumettre';
+$LANG['PERMREMOVECHECK'] = 'Supprimer Liste de Mani&egrave;re Permanente';
+$LANG['REMOVEUSERCHECK'] = 'Avant que la liste puisse &ecirc;tre supprim&acute;e, tous les &eacute;diteurs (sauf vous-m&ecirc;me) et les affectations de projet d\'inventaire doivent &ecirc;tre supprim&eacute;es. 
+                Les attributions de projets d\'inventaire ne peuvent &ecirc;tre supprim&eacute;es que par des gestionnaires actifs du projet ou d\'un administrateur syst&egrave;me.';
+$LANG['WARNINGNOUN'] = 'ATTENTION: Action ne peut pas &ecirc;tre annul&eacute;e.';
+$LANG['CONFIRMDELETE'] = '&Ecirc;tes-vous s&ucirc;r de vouloir supprimer d&eacute;finitivement la liste de contr&ocirc;le? Cette action ne peut pas &ecirc;tre annul&eacute;e!';
+$LANG['CREATECHECKDET'] = 'Cr&eacute;er Nouvelle Liste';
+$LANG['EDITCHECKDET'] = 'Modifier D&eacute;tails de Liste';
+$LANG['CHECKNAME'] = 'Nom de Liste';
+$LANG['CHECKTYPE'] = 'Type de Liste';
+$LANG['RARETHREAT'] = 'Liste d\'esp&egrave;ces rares, menac&eacute;es et prot&acute;g&eacute;es';
+$LANG['GENCHECK'] = 'Liste G&eacute;n&eacute;rale';
+$LANG['LOC'] = 'Localit&eacute;';
+$LANG['PARCHECK'] = 'Liste de Parents';
+$LANG['SELECTPARCHECK'] = 'S&eacute;lectionnez Liste de Parents';
+$LANG['SPECIES'] = 'Esp&eacute;ce:';
+$LANG['LATCENT'] = 'Latitude';
+$LANG['LONGCENT'] = 'Longitude';
+$LANG['POINTRAD'] = 'Rayon de Points (m&egrave;tre)';
+$LANG['POLYFOOT'] = 'Empreinte polygone';
+$LANG['POLYGON_DEFINED'] = 'Empreinte polygone d&acute;finie<br/>Cliquez Globe pour Afficher/Modifier';
+$LANG['POLYGON_NOT_DEFINED'] = 'Empreinte polygone non d&acute;finie<br/>Cliquez Globe pour Cr&eacute;er Polygone';
+$LANG['POLYGON_READY'] = 'Polygone chang&eacute;/enlev&eacute;<br/>et pr&ecirc;t &agrave; &ecirc;tre sauv&eacute;';
+$LANG['DEFAULTDISPLAY'] = 'Param&egrave;tres d\'affichage par d&eacute;faut';
+$LANG['SHOWDETAILS'] = 'Afficher D&eacute;tails';
+$LANG['COMMON'] = 'Afficher des Noms Communs';
+$LANG['NOTESVOUC'] = 'Afficher Notes &amp; &Eacute;chantillons';
+$LANG['TAXONAUTHOR'] = 'Afficher Auteurs de Taxon';
+$LANG['ACTIVATEKEY'] = 'Activer Cl&eacute; d\'Identification';
+$LANG['DEFAULT_SORT'] = 'S&eacute;quence de tri par d&eacute;faut';
+$LANG['ACCESS'] = 'Acc&egrave;s';
+$LANG['PRIVATE'] = 'Priv&eacute;e';
+$LANG['PUBLIC'] = 'Publique';
+$LANG['ADDIMGVOUC'] = 'Ajouter Image d\'&Eacute;chantillon et lien vers Liste';
+$LANG['FORMADDVOUCH'] = 'Ce formulaire vous permettra d\'ajouter un image d\'&eacute;chantillon li&eacute; &agrave; cette liste.
+                        <br/>Si ce n\'est pas d&eacute;j&agrave; pr&eacute;sent, le nom scientifique sera ajout&eacute; &agrave; la liste.';
+$LANG['SELECTVOUCPROJ'] = 'S&eacute;lectionnez le projet d\'&eacute;chantillon auquel vous souhaitez ajouter le &eacute;chantillon. ';
+$LANG['ADDIMGVOUCH'] = 'Ajouter Image d\'&Eacute;chantillon';
+$LANG['IDNOTSET'] = ' Identificateur de liste non d&eacute;fini';
+$LANG['NOADMINPERM'] = ' Vous n\'avez pas d\'autorisation administrative pour cette liste';
+$LANG['CURREDIT'] = '&Eacute;diteurs actuels';
+$LANG['REMOVEEDITPRIVCONFIRM'] = '&Ecirc;tes-vous s&ucirc;r de vouloir supprimer les droits d\'&eacute;dition de cet utilisateur?';
+$LANG['MASSUPDATEED'] = 'Modifier D&eacute;tails de Liste';
+$LANG['ADDEDITOR'] = 'Ajouter un &Eacute;ditor';
+$LANG['DELETECHECK'] = 'Supprimer Liste';
+$LANG['EDITCHECKLIST'] = 'Enregistrer Modifications';
+$LANG['ADDCHECKLIST'] = 'Cr&eacuteer Liste';
+$LANG['DELETETHISU'] = 'Supprimer Cet Utilisateur';
+?>

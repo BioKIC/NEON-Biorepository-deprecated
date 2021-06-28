f98a9b95f15e2466033f85a3c532299ce501e7c3
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-06-28 16:56:47 -0700

    Add French translations

---
+++ content/lang/checklists/dynamicmap.fr.php
@@ -0,0 +1,27 @@
+<?php
+/*
+------------------
+Language: Français (French)
+------------------
+*/
+
+$LANG['CHECKLIST_GENERATOR'] = 'Générateur de Liste Dynamique';
+$LANG['CLICK_MAP'] = "Vous devez d'abord cliquer sur la carte pour capturer les points de coordonnées";
+$LANG['HOME'] = 'Accueil';
+$LANG['DYNAMIC_MAP'] = 'Carte Dynamique';
+$LANG['CAPTURE_COORDS'] = "Faites un panoramique, zoomez et cliquez sur la carte pour capturer les coordonnées, puis soumettez les coordonnées pour créer une liste d'espèces.";
+$LANG['MORE_DETAILS'] = 'Plus de Détails';
+$LANG['RADIUS_DESCRIPTION'] = "Si un rayon est défini, des listes d'espèces sont générées à l'aide de données de spécimens collectées dans la zone définie.
+					Si un rayon n'est pas fourni, la zone est échantillonnée en anneaux concentriques jusqu'à ce que la taille de l'échantillon soit déterminée à 
+					représentent le mieux la diversité des espèces locales. En d'autres termes, les zones mal collectées auront un plus grand rayon échantillonné.
+					La définition du filtre taxonomique limitera le retour aux espèces trouvées dans ce groupe taxonomique.";
+$LANG['LESS_DETAILS'] = 'Moins de Détails';
+$LANG['BUILD_CHECKLIST'] = 'Construire Liste';
+$LANG['POINT'] = 'Point (Lat, Long)';
+$LANG['CLICK_MAP'] = 'Cliquez sur Carte';
+$LANG['TAXON_FILTER'] = 'Filtre de Taxon';
+$LANG['RADIUS'] = 'Rayon';
+$LANG['KM'] = 'Kilomètres';
+$LANG['MILES'] = 'Milles';
+
+?>
---
+++ content/lang/checklists/index.fr.php
@@ -0,0 +1,14 @@
+<?php
+/*
+------------------
+Language: Français (French)
+------------------
+*/
+
+$LANG['NAV_HOME'] = 'Accueil';
+$LANG['SPECIES_INVENTORIES'] = 'Inventaires des Espèces';
+$LANG['SHOW_MAP'] = 'Afficher Inventaires sur Carte';
+$LANG['NO_INVENTORIES'] = 'Aucun inventaire retourné';
+$LANG['MISC_INVENTORIES'] = 'Inventaires Divers';
+
+?>

d860480b0d6c1ea6ee39f8308afa3adeeabd74a2
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-07-22 15:49:49 -0700

    Fix bugs and add language tags

---
+++ content/lang/collections/misc/assocmanagement.en.php
@@ -0,0 +1,19 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+include_once($SERVER_ROOT.'/content/lang/collections/misc/sharedterms.'.$LANG_TAG.'.php');
+
+$LANG['OCC_BATCH'] = 'Occurrence Association Batch Build';
+$LANG['OCC_AS_MAN'] = 'Occurrence Association Manager';
+$LANG['TAXA_PARSING'] = 'Associated Taxa Parsing';
+$LANG['NUM_PARSED'] = 'Number of parsed specimens';
+$LANG['NUM_UNPARSED'] = 'Number of unparsed specimens';
+$LANG['NON_INDEXED'] = 'Number of non-indexed parsing terms';
+$LANG['SPEC_RECS'] = 'specimen records';
+$LANG['PERM_PROB'] = 'ERROR: permissions failure';
+
+?>
---
+++ content/lang/collections/misc/sharedterms.en.php
@@ -0,0 +1,13 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+$LANG['HOME'] = 'Home';
+$LANG['DETAILS'] = 'Details';
+$LANG['QUICK_SEARCH'] = 'Quick Search';
+$LANG['CAT_NUM'] = 'Catalog Number';
+
+?>

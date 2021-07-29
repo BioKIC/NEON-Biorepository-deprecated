41c7aafb84263dfb5201cfe8881cdade6c2d1c69
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-07-29 15:10:40 -0700

    Bug fixes

--- content/lang/collections/misc/collorderstats.en.php
+++ content/lang/collections/misc/collorderstats.en.php
@@ -9,7 +9,7 @@ $LANG['ORDER_DIST'] = 'Order Distribution';
 $LANG['ORDER'] = 'Order';
 $LANG['SPECIMENS'] = 'Specimens';
 $LANG['SPECIES_ID'] = "Species ID";
-$LANG['GEOREFERENCED']:'Georeferenced'
+$LANG['GEOREFERENCED'] = 'Georeferenced';
 $LANG['AND'] = 'and';
 $LANG['SPEC_W_ORDER'] = 'Total Specimens with Order';
 $LANG['NO_ORDER'] = 'Specimens without Order';

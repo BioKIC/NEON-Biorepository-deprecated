e2791795d8f8ec98a3ffcd21f7fc9980b8bffd23
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-07-29 15:09:40 -0700

    Fixed lang tag files

--- content/lang/collections/misc/collorderstats.en.php
+++ content/lang/collections/misc/collorderstats.en.php
@@ -1,38 +1,19 @@
 <?php
 /*
 ------------------
-Language: Français (French)
+Language: English
 ------------------
 */
 
-include_once($SERVER_ROOT.'/content/lang/collections/misc/sharedterms.'.$LANG_TAG.'.php');
-
-$LANG['LINK_RESOURCE'] = 'Liste des Ressources Liées';
-$LANG['ENGLISH'] = 'Anglais';
-$LANG['SPANISH'] = 'Espagnol';
-$LANG['FRENCH'] = 'Français';
-$LANG['PORTUGUESE'] = 'Portugais';
-$LANG['TITLE'] = 'Titre';
-$LANG['NO_LINKS'] = "Aucun lien n'a encore été défini";
-$LANG['ADD_EDIT_LINK'] = 'Ajouter/Modifier Ressource de Lien Externe';
-$LANG['CAPTION_OVERRIDE'] = 'Remplacement de Légende';
-$LANG['ADD_LINK'] = 'Ajouter Lien';
-$LANG['APPLY_EDITS'] = 'Appliquer Modifications';
-$LANG['CONTACTS'] = 'Contacts';
-$LANG['ROLE'] = 'Rôle';
-$LANG['EMAIL'] = 'Email';
-$LANG['C_CONTACT'] = 'contact central';
-$LANG['ORCID'] = '# ORCID';
-$LANG['PHONE'] = 'Téléphoner';
-$LANG['NO_CONTACTS'] = "Aucun contact n'a encore été défini";
-$LANG['ADD_EDIT_CONTACT'] = 'Ajouter/Modifier Contact';
-$LANG['FIRST_NAME'] = 'Prénom';
-$LANG['LAST_NAME'] = 'Nom de famille';
-$LANG['IS_C_CONTACT'] = 'est contact central';
-$LANG['ADD_CONTACT'] = 'Ajouter Contact';
-$LANG['EDIT_CONTACT'] = 'Modifier Contact';
-$LANG['RESET_FORM'] = 'Réinitialiser Formulaire';
-$LANG['FIRST_NAME_REQ'] = "Prénom est un champ obligatoire";
-$LANG['LAST_NAME_REQ'] = 'Nom de famille est un champ obligatoire';
+$LANG['ORDER_DIST'] = 'Order Distribution';
+$LANG['ORDER'] = 'Order';
+$LANG['SPECIMENS'] = 'Specimens';
+$LANG['SPECIES_ID'] = "Species ID";
+$LANG['GEOREFERENCED']:'Georeferenced'
+$LANG['AND'] = 'and';
+$LANG['SPEC_W_ORDER'] = 'Total Specimens with Order';
+$LANG['NO_ORDER'] = 'Specimens without Order';
+$LANG['SAVE_CSV'] = 'Save CSV';
+$LANG['DOWNLOAD_ORDER'] = 'Download Order Dist';
 
 ?>
--- content/lang/collections/misc/collorderstats.fr.php
+++ content/lang/collections/misc/collorderstats.fr.php
@@ -9,6 +9,7 @@ $LANG['ORDER_DIST'] = 'Répartition des Ordres';
 $LANG['ORDER'] = 'Ordre';
 $LANG['SPECIMENS'] = 'Spécimens';
 $LANG['SPECIES_ID'] = "ID de Espèce";
+$LANG['GEOREFERENCED']:'Géoréférencé'
 $LANG['AND'] = 'et';
 $LANG['SPEC_W_ORDER'] = "Nombre total d'échantillons avec Ordre";
 $LANG['NO_ORDER'] = 'Spécimens sans Ordre';

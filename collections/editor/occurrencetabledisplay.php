75d730b92c4669a432ccedbecea12b9c25ee39a8
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-08-02 13:20:28 -0700

    Update occurrencetabledisplay.php

--- collections/editor/occurrencetabledisplay.php
+++ collections/editor/occurrencetabledisplay.php
@@ -150,7 +150,7 @@ else{
 		if(($isEditor || $crowdSourceMode)){
 			?>
 			<div style="text-align:right;width:790px;margin:-30px 15px 5px 0px;">
-				<button title="Search / Filter" onclick="toggleSearch();return false;">Search/Filter</button>
+				<button title="Search / Filter" onclick="toggleSearch();return false;">Record Search Form</button>
 				<?php
 				if($isEditor == 1 || $isGenObs){
 					?>

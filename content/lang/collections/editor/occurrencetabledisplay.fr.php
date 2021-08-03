94a5dfd651d5ece57e4ae84cdc788b3f5f18fe09
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-08-03 14:15:28 -0700

    Create occurrencetabledisplay.fr.php

---
+++ content/lang/collections/editor/occurrencetabledisplay.fr.php
@@ -0,0 +1,43 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+$LANG['PREVIOUS'] = 'Précédents';
+$LANG['RECORDS'] = 'enregistrements';
+$LANG['OF'] = 'des';
+$LANG['NEXT'] = 'Prochains';
+$LANG['TABLE_VIEW'] = 'Vue du Tableau des Occurrences';
+$LANG['SEARCH_FORM'] = 'Formulaire de Recherche';
+$LANG['BATCH_UPDATE'] = 'Mise à Jour Par Lots';
+$LANG['FIELD_NAME'] = 'Nom du Champ';
+$LANG['SELECT_FIELD'] = 'Sélectionnez Nom du Champ';
+$LANG['CURRENT_VALUE'] = 'Valeur Actuelle';
+$LANG['NEW_VALUE'] = 'Nouvelle Valeur';
+$LANG['UNPROCESSED'] = 'Non Traité';
+$LANG['UNPROCESSED_NLP'] = 'Non Traité/NLP';
+$LANG['STAGE_1'] = 'Étape 1';
+$LANG['STAGE_2'] = 'Étape 2';
+$LANG['STAGE_3'] = 'Étape 3';
+$LANG['PENDING_NFN'] = 'En Attente d\'Examen-NfN';
+$LANG['PENDING_REVIEW'] = 'En Attente d\'Examen';
+$LANG['EXPERT_REQUIRED'] = 'Expert Requis';
+$LANG['REVIEWED'] = 'Révisé';
+$LANG['CLOSED'] = 'Fermé';
+$LANG['NO_STATUS'] = 'Aucun État Défini';
+$LANG['MATCH_WHOLE'] = 'Faire Correspondre Champ Entier';
+$LANG['MATCH_PART'] = 'Faire Correspondre n\Importe Quelle Partie du Champ';
+$LANG['BATCH_UP_FIELD'] = 'Champ de Nise à Jour Par Lots';
+$LANG['HOME'] = 'Accueil';
+$LANG['CENTRAL_CROWD'] = 'Crowdsourcing Central';
+$LANG['COL_MANAGEMENT'] = 'Gestion des Collections';
+$LANG['PERS_MANAGEMENT'] = 'Gestion du Personnel';
+$LANG['SYMB_ID'] = 'ID Symbiota';
+$LANG['NEW_WINDOW'] = 'ouvrir dans une nouvelle fenêtre';
+$LANG['CLICK_ID'] = "Cliquez sur l'identifiant Symbiota dans la première colonne pour ouvrir l'éditeur.";
+$LANG['NONE_FOUND'] = 'Aucun enregistrement trouvé correspondant à la requête';
+$LANG['NOT_AUTH'] = "Vous n'êtes pas autorisé à accéder à cette page";
+
+?>

e9aeee81e3f86c398d2de1e64cc63bdc37ec8b16
Edward Gilbert <egbot@asu.edu>
2021-08-09 08:33:08 -0700

    Multi-language support development
    - Add Spanish placeholder files containing English translations until the Spanish files are made available.

---
+++ content/lang/checklists/dynamicmap.es.php
@@ -0,0 +1,27 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+$LANG['CHECKLIST_GENERATOR'] = 'Dynamic Checklist Generator';
+$LANG['CLICK_MAP'] = 'You must first click on map to capture coordinate points';
+$LANG['HOME'] = 'Home';
+$LANG['DYNAMIC_MAP'] = 'Dynamic Map';
+$LANG['CAPTURE_COORDS'] = 'Pan, zoom and click on map to capture coordinates, then submit coordinates to build a species list.';
+$LANG['MORE_DETAILS'] = 'More Details';
+$LANG['RADIUS_DESCRIPTION'] = 'If a radius is defined, species lists are generated using specimen data collected within the defined area.
+					If a radius is not supplied, the area is sampled in concentric rings until the sample size is determined to 
+					best represent the local species diversity. In other words, poorly collected areas will have a larger radius sampled. 
+					Setting the taxon filter will limit the return to species found within that taxonomic group.';
+$LANG['LESS_DETAILS'] = 'Less Details';
+$LANG['BUILD_CHECKLIST'] = 'Build Checklist';
+$LANG['POINT'] = 'Point (Lat, Long)';
+$LANG['CLICK_MAP'] = 'Click on map';
+$LANG['TAXON_FILTER'] = 'Taxon Filter';
+$LANG['RADIUS'] = 'Radius';
+$LANG['KM'] = 'Kilometers';
+$LANG['MILES'] = 'Miles';
+
+?>

62c404b421b59b34e602709fba5f580190bb5ee9
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-08-10 10:31:00 -0700

    Update vaconflicts.es.php

--- content/lang/checklists/vaconflicts.es.php
+++ content/lang/checklists/vaconflicts.es.php
@@ -6,8 +6,7 @@ Language: Español (Spanish)
 */
 
 $LANG['SELECT_ONE'] = 'Al menos un voucher necesita ser seleccionado';
-$LANG['POSS_CONFLICTS'] = 'Posibles Vouchers Conflictivos';
-$LANG['POSS_CONFLICTS'] = 'Listado de especímenes voucher cuya identificación actual está en conflicto con el listado de especies. 
+$LANG['EXPLAIN_PARAGRAPH'] = 'Listado de especímenes voucher cuya identificación actual está en conflicto con el listado de especies. 
 		Los conflictos de voucher se deben típicamente a anotaciones recientes de especímenes localizados dentro de colecciones. 
 		Hacer click en ID del Listado de Especies para abrir el panel de edición para ese registro.';
 $LANG['CHECK_ID'] = 'ID del Listado de Especies';

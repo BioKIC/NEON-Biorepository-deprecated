50ac21aa2d09515c3b1a1da64988552f4f5b00e0
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-08-17 10:46:10 -0700

    Lang tags for observationsubmit

---
+++ content/lang/collections/editor/observationsubmit.en.php
@@ -0,0 +1,69 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+include_once($SERVER_ROOT.'/content/lang/collections/editor/skeletalsubmit.'.$LANG_TAG.'.php')
+
+$LANG['OBS_SUBMIT'] = 'Observation Submission';
+$LANG['SUCCESS_IMAGE'] = 'SUCCESS: Image loaded successfully!';
+$LANG['OPEN'] = 'Open';
+$LANG['OCC_DET_VIEW'] = 'Occurrence Details Viewer';
+$LANG['TO_SEE_NEW'] = 'to see the new record';
+$LANG['GO_TO'] = 'Go to';
+$LANG['CHECKLIST'] = 'checklist';
+$LANG['ERROR'] = 'ERROR';
+$LANG['UNK_ERROR'] = 'UNKNOWN ERROR: image file may have been larger than allowed by server. Try uploading a smaller image or have system administrator modify PHP configurations';
+$LANG['FIELD_NAMES_REQ'] = 'Field names in <b>bold</b> are required';
+$LANG['IMAGES'] = 'Images';
+$LANG['IMAGE'] = 'Image';
+$LANG['CAPTION'] = 'Caption';
+$LANG['IMG_REMARKS'] = 'Image Remarks';
+$LANG['ADD_ANOTHER'] = 'Add Another Image';
+$LANG['OBSERVATION'] = 'Observation';
+$LANG['OBSERVER'] = 'Observer';
+$LANG['NUMBER'] = 'Number';
+$LANG['ASSOC_OBSERVERS'] = 'Associated Observers';
+$LANG['IDED_BY'] = 'Identified By';
+$LANG['DATE_IDED'] = 'Date Identified';
+$LANG['ID_REFS'] = 'ID References';
+$LANG['ID_REMARKS'] = 'ID Remarks';
+$LANG['LOCALITY'] = 'Locality';
+$LANG['HIDE_LOC_SHORT'] = 'Hide Locality Data from General Public';
+$LANG['HIDE_LOC_LONG'] = 'Hide Locality Details from General Public (rare, threatened, or sensitive species)';
+$LANG['LATITUDE'] = 'Latitude';
+$LANG['LONGITUDE'] = 'Longitude';
+$LANG['DMS'] = 'DMS';
+$LANG['UNCERTAINTY_M'] = 'Uncertainty(m)';
+$LANG['DATUM'] = 'Datum';
+$LANG['ELEV_M'] = 'Elev. (meters)';
+$LANG['ELEV_FT'] = 'Elev. (feet)';
+$LANG['GEO_REMARKS'] = 'Georeference Remarks';
+$LANG['LATITUDE_DEG'] = 'Latitude Degrees';
+$LANG['LATITUDE_MIN'] = 'Latitude Minutes';
+$LANG['LATITUDE_SEC'] = 'Latitude Seconds';
+$LANG['N'] = 'N'; //As in North
+$LANG['S'] = 'S'; //As in South
+$LANG['LONGITUDE_DEG'] = 'Longitude Degrees';
+$LANG['LONGITUDE_MIN'] = 'Longitude Minutes';
+$LANG['LONGITUDE_SEC'] = 'Longitude Seconds';
+$LANG['E'] = 'E'; //As in East
+$LANG['W'] = 'W'; //As in West
+$LANG['MISC'] = 'Misc';
+$LANG['HABITAT'] = 'Habitat';
+$LANG['SUBSTRATE'] = 'Substrate';
+$LANG['ASSOC_TAXA'] = 'Associated Taxa';
+$LANG['DESC_ORG'] = 'Description of Organism';
+$LANG['GENERAL_NOTES'] = 'General Notes';
+$LANG['REP_COND'] = 'Reproductive Condition';
+$LANG['EST_MEANS'] = 'Establishment Means';
+$LANG['CULT_CAPT'] = 'Cultivated/Captive';
+$LANG['LINK_CHECK'] = 'Link to Checklist as Voucher';
+$LANG['SP_LIST'] = 'Species List';
+$LANG['SEL_CHECKLIST'] = 'Select Checklist';
+$LANG['SUBMIT_OBS'] = 'Submit Observation';
+$LANG['NOT_AUTH'] = 'You are not authorized to submit to an observation.';
+
+?>

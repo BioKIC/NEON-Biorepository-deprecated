e4b017cca86063135e928792ac84de980bc62637
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-09-08 15:06:36 -0700

    Add lang tags to batchgeoref, French translations

---
+++ content/lang/collections/editor/skeletalsubmit.fr.php
@@ -0,0 +1,60 @@
+<?php
+/*
+------------------
+Language: Français (French)
+------------------
+*/
+
+$LANG['OCC_SKEL_SUBMIT'] = 'Soumission du Dossier Squelettique';
+$LANG['HOME'] = 'Accueil';
+$LANG['COL_MNGMT'] = 'Gestion des Collections';
+$LANG['DISPLAY_INSTRUCTIONS'] = 'Afficher Instructions';
+$LANG['SKELETAL_DATA'] = 'Données Squelettiques';
+$LANG['DISPLAY_OPTIONS'] = 'Option d\'Affichage';
+$LANG['TOOL_DESCRIPTION'] = 'Description de Outil';
+$LANG['SKELETAL_DESCIPRTION_1'] = "Cette page est généralement utilisée pour saisir les enregistrements squelettiques dans le système pendant le processus d'imagerie. 
+						Étant donné que les collections sont généralement organisées par nom scientifique, pays et état, il faut peu d'efforts supplémentaires aux équipes d'imagerie pour collecter ces informations pendant qu'elles imagent des spécimens. 
+						L'équipe d'imagerie saisit les informations de collecte de base partagées par le lot d'échantillons en cours de traitement, puis chaque fois qu'elle scanne un code-barres dans le champ du numéro de catalogue, un enregistrement est ajouté au système avec le numéro de catalogue et les données squelettiques.";
+$LANG['SKELETAL_DESCIPRTION_2'] = "Des données plus complètes peuvent être saisies en cliquant sur le numéro de catalogue, mais le flux de travail recommandé consiste à traiter les données complètes de l'étiquette directement à partir de l'image de l'étiquette du spécimen à un stade ultérieur. 
+						Une image peut également être téléchargée en cliquant sur le symbole de l'image à droite du numéro de catalogue, mais les images doivent généralement être ajustées avant d'être prêtes pour le téléchargement (par exemple, redimensionnées, équilibrées en lumière). 
+						De plus, les projets qui stockent leurs images sur des serveurs d'images distants nécessitent généralement des flux de travail séparés pour le traitement par lots des images. 
+						Contactez votre gestionnaire de projet/portail pour connaître la manière préférée de charger des images d'échantillons.";
+$LANG['SKELETAL_DESCIPRTION_3'] = "Cliquez sur le symbole Option d'affichage situé au-dessus du nom scientifique pour ajuster l'affichage du champ et l'action préférée lorsqu'un enregistrement existe déjà pour un numéro de catalogue. 
+						Par défaut, une nouvelle notice ne sera pas créée si le numéro de catalogue existe déjà. 
+						Cependant, une option secondaire est disponible qui ajoutera des données squelettiques dans les champs vides des enregistrements existants. 
+						Les données squelettiques ne seront pas copiées sur les valeurs de champ existantes.";
+$LANG['OPTIONS'] = 'Options';
+$LANG['X_CLOSE'] = 'X Fermer';
+$LANG['FIELD_DISPLAY'] = 'Affichage du Champ';
+$LANG['OTHER_CAT_NUMS'] = 'Autres Numéros de Catalogue';
+$LANG['AUTHOR'] = 'Auteur';
+$LANG['FAMILY'] = 'Famille';
+$LANG['LOCALITY_SECURITY'] = 'Sécurité de Localité';
+$LANG['COUNTRY'] = 'Pays';
+$LANG['STATE_PROVINCE'] = 'État/Province';
+$LANG['COUNTY_PARISH'] = 'Comté/Paroisse';
+$LANG['COLLECTOR'] = 'Collectionneur';
+$LANG['COLLECTOR_NO'] = 'Numéro de Collectionneur';
+$LANG['COLLECTION_DATE'] = 'Date de Collecte';
+$LANG['LABEL_PROJECT'] = "Projet d'Étiquette";
+$LANG['PROCESSING_STATUS'] = 'Statut de Traitement';
+$LANG['LANGUAGE'] = 'Langue';
+$LANG['CATNUM_MATCH'] = 'Action de Correspondance des Numéros de Catalogue';
+$LANG['RESTRICT_IF_EXISTS'] = 'Restreindre Entrée si Enregistrement existe';
+$LANG['APPEND_VALUES'] = 'Ajouter des vValeurs aux Enregistrements Existants';
+$LANG['SESSION'] = 'Session';
+$LANG['COUNT'] = 'Total';
+$LANG['RATE'] = 'Taux';
+$LANG['PER_HOUR'] = 'par heure';
+$LANG['SCINAME'] = 'Nom Scientifique';
+$LANG['ADD_NAME_THESAURUS'] = 'Ajouter un nouveau nom au thésaurus taxonomique';
+$LANG['PROTECT_LOCALITY'] = 'Protéger détails de localité du grand public';
+$LANG['DATE'] = 'Date';
+$LANG['CATALOGNUMBER'] = 'Numéro de Catalogue';
+$LANG['ADD_RECORD'] = 'Ajouter Enregistrement';
+$LANG['RECORDS'] = 'Enregistrements';
+$LANG['NOT_AUTHORIZED'] = "Vous n'êtes pas autorisé à accéder à cette page.";
+$LANG['CONTACT_ADMIN'] = 'Contactez un administrateur pour obtenir autorisations nécessaires';
+$LANG['ERROR_NO_ID'] = 'ERREUR : identifiant de collection non défini';
+
+?>

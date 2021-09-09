baad87c699ebd195781e40f26131cfb5062daaa0
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-09-09 10:56:53 -0700

    Update occurrencetabledisplay.php
    Add Data Generalizations to the options for batch update

--- collections/editor/occurrencetabledisplay.php
+++ collections/editor/occurrencetabledisplay.php
@@ -44,7 +44,8 @@ $headerMapBase = array('institutioncode'=>'Institution Code (override)','collect
 	'samplingprotocol' => 'Sampling Protocol', 'preparations' => 'Preparations', 'reproductivecondition' => 'Reproductive Condition',
 	'typestatus' => 'Type Status','cultivationstatus' => 'Cultivation Status','establishmentmeans' => 'Establishment Means',
 	'disposition' => 'Disposition','duplicatequantity' => 'Duplicate Qty','datelastmodified' => 'Date Last Modified', 'labelproject' => 'Project',
-	'processingstatus' => 'Processing Status','recordenteredby' => 'Entered By','dbpk' => 'dbpk','basisofrecord' => 'Basis Of Record','language' => 'Language');
+	'processingstatus' => 'Processing Status','recordenteredby' => 'Entered By','dbpk' => 'dbpk','basisofrecord' => 'Basis Of Record',
+	'language' => 'Language', 'datageneralizations' => 'Data Generalizations');
 $headMap = array();
 
 $qryCnt = 0;

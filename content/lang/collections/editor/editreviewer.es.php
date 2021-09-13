b1dc567372b4755522680b16c9f4bc2613392d09
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-09-13 08:27:56 -0700

    Added Spanish translations of collections/editor

---
+++ content/lang/collections/editor/editreviewer.es.php
@@ -0,0 +1,57 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+$LANG['EDIT_REVIEWER'] = 'Revisor de Edición de Especímenes';
+$LANG['DATE_PROBLEM'] = 'Fecha inicial no puede ser posterior a la fecha final';
+$LANG['PLEASE_CHECK_EDIT'] = '¡Por favor revisar al menos una edición del listado de abajo!';
+$LANG['SURE_DELETE_HISTORY'] = '¿Está seguro que quiere remover permanentemente las ediciones seleccionadas de la historia?';
+$LANG['PERS_SPEC_MNG'] = 'Manejo Personal de Especímenes';
+$LANG['COL_MAN_PAN'] = 'Panel de Manejo de Colecciones';
+$LANG['FILTER'] = 'Filtrar';
+$LANG['APPLIED_STATUS'] = 'Estado Aplicado:';
+$LANG['ALL_RECS'] = 'Todos los Registros';
+$LANG['NOT_APPLIED'] = 'No Aplicado';
+$LANG['APPLIED'] = 'Aplicado';
+$LANG['REVIEW_STATUS'] = 'Revisar Estado';
+$LANG['OPEN_PENDING'] = 'Abierto/Pendiente';
+$LANG['OPEN_ONLY'] = 'Únicamente Abiertos';
+$LANG['PENDING_ONLY'] = 'Únicamente Pendientes';
+$LANG['CLOSED'] = 'Cerrado';
+$LANG['EDITOR'] = 'Editor';
+$LANG['ALL_EDITORS'] = 'Todos los Editores';
+$LANG['DATE'] = 'Fecha';
+$LANG['TO'] = 'para';
+$LANG['EDITING_SOURCE'] = 'Fuente de Edición';
+$LANG['INTERNAL'] = 'Interno';
+$LANG['EXTERNAL'] = 'Externo';
+$LANG['SUBMIT_FILTER'] = 'Enviar Filtro';
+$LANG['ACTION_PANEL'] = 'Panel de Acción';
+$LANG['APPLY_EDITS_IF'] = 'Aplicar Ediciones, si aún no se ha realizado';
+$LANG['APPLY_EDITS'] = 'Aplicar Ediciones';
+$LANG['REVERT_EDITS'] = 'Revertir Ediciones';
+$LANG['REVIEW_STATUS'] = 'Revisar Estado';
+$LANG['LEAVE_AS_IS'] = 'DEJAR COMO ESTÁ';
+$LANG['OPEN'] = 'ABIERTO';
+$LANG['PENDING'] = 'PENDIENTE';
+$LANG['C_CLOSED'] = 'CERRADO';
+$LANG['UPDATE_SELECTED'] = 'Actualizar Registros Seleccionados';
+$LANG['ADDITIONAL_ACTIONS'] = 'Acciones Adicionales';
+$LANG['PERMANENTLY_CLEAR'] = 'Eliminar permanentemente las ediciones seleccionadas de la historia de versiones. Advertencia: ¡esta acción no puede revertirse!';
+$LANG['DOWNLOAD_SELECTED'] = 'Descargar Registros Seleccionados';
+$LANG['BASED_ON_PARAMETERS'] = 'Basado en parámetros de búsqueda en el Panel de Filtros de la derecha';
+$LANG['PRINT_FRIENDLY'] = 'Imprimir Página Amigable';
+$LANG['RECORD_NO'] = '# de Registro';
+$LANG['CAT_NUM'] = 'Número de Catálogo';
+$LANG['TIMESTAMP'] = 'Marca de Tiempo';
+$LANG['FIELD_NAME'] = 'Nombre del Campo';
+$LANG['OLD_VALUE'] = 'Valor Anterior';
+$LANG['NEW_VALUE'] = 'Valor Nuevo';
+$LANG['UNKNOWN'] = 'DESCONOCIDO';
+$LANG['NONE_FOUND'] = 'No existen Ediciones que coincidan con el criterio de búsqueda.';
+$LANG['ERROR'] = '¡Error!';
+
+?>
---
+++ content/lang/collections/editor/skeletalsubmit.es.php
@@ -0,0 +1,64 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+$LANG['OCC_SKEL_SUBMIT'] = 'Envío de Registros Esqueléticos de Ocurrencia';
+$LANG['HOME'] = 'Inicio';
+$LANG['COL_MNGMT'] = 'Manejo de Colecciones';
+$LANG['DISPLAY_INSTRUCTIONS'] = 'Desplegar Instrucciones';
+$LANG['SKELETAL_DATA'] = 'Datos Esqueléticos';
+$LANG['DISPLAY_OPTIONS'] = 'Desplegar Opciones';
+$LANG['TOOL_DESCRIPTION'] = 'Descripción de la Herramienta';
+$LANG['SKELETAL_DESCIPRTION_1'] = 'Esta página es típicamente utilizada para ingresar registros esqueléticos en el sistema durante el proceso de subir imágenes. Ya que las colecciones están
+						comúnmente organizadas por nombre científico, país, y estado, toma un poco de esfuerzo adicional para que los equipos de fotografía
+						recolecten esta información mientras están generando las imágenes de los especímenes. El equipo de fotografía entra la información básica de
+						colección compartida por el conjunto de especímenes que están siendo procesados, luego cada vez que escanean un código de barras en el campo de
+						número de catálogo, un registro es añadido al sistema con el número de catálogo como dato esquelético.';
+$LANG['SKELETAL_DESCIPRTION_2'] = 'Datos más completos pueden ser ingresados al hacer click en el número de catálogo, pero el flujo de trabajo recomendado es procesar los datos completos de la
+						etiqueta directamente de la imagen del espécimen en una etapa posterior. Una imagen también puede ser añadida haciendo click en el símbolo de
+						imagen a la derecha del número de catálogo, pero las  imágenes típicamente necesitan ser ajustadas antes de estar listas para publlicar (e.g. ajuste de tamaño e iluminación).
+						Además, los proyectos que almacenan sus imágenes en servidores remotos 
+						típicamente requerirán de flujos de trabajo separados para procesar imágenes por lote. Contacte al administrador del proyecto / portal para averiguar
+						las formas preferidas para cargar imágenes de especímenes.';
+$LANG['SKELETAL_DESCIPRTION_3'] = 'Hacer click en el símbolo de Desplegar Opciontes ubicado sobre el nombre científico para ajustar el despliegue de campos y acciones preferidas cuando un registro
+						ya exista para un número de catálogo. Por defecto, un nuevo registro no será creado si el número de catálogo ya existe.
+						Sin embargo, una opción secundaria está disponible para añadir datos esqueléticos en campos vacíos de registros existentes.
+						Los datos esqueléticos no serán copiados sobre campos con valores existentes.';
+$LANG['OPTIONS'] = 'Opciones';
+$LANG['X_CLOSE'] = 'X Cerrar';
+$LANG['FIELD_DISPLAY'] = 'Despliegue de Campos';
+$LANG['OTHER_CAT_NUMS'] = 'Otros Números de Catálogo';
+$LANG['AUTHOR'] = 'Autor';
+$LANG['FAMILY'] = 'Familia';
+$LANG['LOCALITY_SECURITY'] = 'Seguridad de Localidad';
+$LANG['COUNTRY'] = 'País';
+$LANG['STATE_PROVINCE'] = 'Estado/Provincia';
+$LANG['COUNTY_PARISH'] = 'Condado/Poblado';
+$LANG['COLLECTOR'] = 'Colector';
+$LANG['COLLECTOR_NO'] = 'Número de Colector';
+$LANG['COLLECTION_DATE'] = 'Fecha de Colecta';
+$LANG['LABEL_PROJECT'] = 'Etiqueta del Proyecto';
+$LANG['PROCESSING_STATUS'] = 'Estado de Procesamiento';
+$LANG['LANGUAGE'] = 'Idioma';
+$LANG['CATNUM_MATCH'] = 'Evaluación de Coincidencia de Números de Catálogo';
+$LANG['RESTRICT_IF_EXISTS'] = 'Restringir entrada si el registro existe';
+$LANG['APPEND_VALUES'] = 'Añadir valores a registros existentes';
+$LANG['SESSION'] = 'Sesión';
+$LANG['COUNT'] = 'Conteo';
+$LANG['RATE'] = 'Proporción';
+$LANG['PER_HOUR'] = 'por hora';
+$LANG['SCINAME'] = 'Nombre Científico';
+$LANG['ADD_NAME_THESAURUS'] = 'Añadir nuevo nombre al tesauro taxonómico';
+$LANG['PROTECT_LOCALITY'] = 'Proteger detalles de localidad del público en general';
+$LANG['DATE'] = 'Fecha';
+$LANG['CATALOGNUMBER'] = 'Número de Catálogo';
+$LANG['ADD_RECORD'] = 'Añadir Registros';
+$LANG['RECORDS'] = 'Registros';
+$LANG['NOT_AUTHORIZED'] = 'No está autorizado para acceder a esta página.';
+$LANG['CONTACT_ADMIN'] = 'Contactar a un administrador para obtener los permisos necesarios';
+$LANG['ERROR_NO_ID'] = 'ERROR: identificador de colección no establecido';
+
+?>

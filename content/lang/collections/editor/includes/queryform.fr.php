a56131295b2900dd8849f853fe13e7c4ebd68c8a
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-09-14 15:59:01 -0700

    Create queryform.fr.php

---
+++ content/lang/collections/editor/includes/queryform.fr.php
@@ -0,0 +1,67 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+$LANG['RECORD_SEARCH_FORM'] = "Formulaire de Recherche d'Enregistrement";
+$LANG['WILD_EXPLAIN'] = "Nom complet du collecteur tel qu'il est entré dans la base de données. Pour rechercher uniquement sur le nom de famille, placez le caractère générique (%) avant le nom (%Gentry).";
+$LANG['COLLECTOR'] = 'Collectionneur';
+$LANG['SEPARATE_RANGES'] = 'Séparez plusieurs termes par une virgule et plages par ' - ' (espace avant et après tiret requis), par exemple: 3542,3602,3700 - 3750';
+$LANG['NUMBER'] = 'Nombre';
+$LANG['ENTER_RANGES'] = 'Entrer des plages séparées par ' - ' (espace avant et après tiret requis), par exemple: 2002-01-01 - 2003-01-01';
+$LANG['DATE'] = 'Date';
+$LANG['CAT_NUM'] = 'Numéro de Catalogue';
+$LANG['OCR_FRAGMENT'] = 'Fragment OCR';
+$LANG['OTHER_CAT_NUMS'] = 'Autres Numéros de Catalogue';
+$LANG['ENTERED_BY'] = 'Entré par';
+$LANG['LIMIT_TO_CURRENT'] = 'Limiter aux enregistrements récents saisis par utilisateur actuel';
+$LANG['CU'] = 'UA'; //as in "Current User"
+$LANG['DATE_ENTERED'] = 'Date Entrée';
+$LANG['DATE_MODIFIED'] = 'Date Modifiée';
+$LANG['PROC_STATUS'] = 'Statut de Traitement';
+$LANG['ALL_RECORDS'] = 'Tous Enregistrements';
+$LANG['NO_SET_STATUS'] = 'Aucun État Défini';
+$LANG['WITH_IMAGES'] = 'avec des images';
+$LANG['WITHOUT_IMAGES'] = 'sans image';
+$LANG['ENTER_EXS_TITLE'] = 'Entrez Titre Exsiccati';
+$LANG['EXS_TITLE'] = 'Titre Exsiccati';
+$LANG['FAMILY'] = 'Famille';
+$LANG['SCI_NAME'] = 'Nom Scientifique';
+$LANG['COUNTRY'] = 'Pays';
+$LANG['STATE_PROVINCE'] = 'État/Province';
+$LANG['COUNTY'] = 'Comté';
+$LANG['MUNICIPALITY'] = 'Municipalité';
+$LANG['COL_NUMBER'] = 'Numéro de Collectionneur';
+$LANG['COL_DATE'] = 'Date de Collecte';
+
+$LANG['CUSTOM_FIELD_1'] = 'Champ Personnalisé 1';
+$LANG['SELECT_FIELD_NAME'] = 'Sélectionnez Nom du Champ';
+$LANG['EQUALS'] = 'ÉQUIVAUT À';
+$LANG['NOT_EQUALS'] = 'PAS ÉGAL';
+$LANG['STARTS_WITH'] = 'COMMENCE AVEC';
+$LANG['CONTAINS'] = "CONTIENT";
+$LANG['DOESNT_CONTAIN'] = "NE CONTIENT PAS";
+$LANG['GREATER_THAN'] = 'PLUS GRAND QUE';
+$LANG['LESS_THAN'] = 'MOINS QUE';
+$LANG['IS_NULL'] = 'EST NULL';
+$LANG['IS_NOT_NULL'] = 'EST NON NULLE';
+$LANG['CUSTOM_FIELD_2'] = 'Champ Personnalisé 2';
+$LANG['CUSTOM_FIELD_3'] = 'Champ Personnalisé 3';
+$LANG['SHOW_RECS_ALL'] = 'Afficher enregistrements pour tous utilisateurs (contrôle administrateur)';
+$LANG['GO_LABEL_PRINT'] = "Aller au Module d'Impression d'Étiquettes";
+$LANG['DISPLAY_EDITOR'] = 'Éditeur d\'Affichage';
+$LANG['DISPLAY_TABLE'] = 'Tableau d\'Affichage';
+$LANG['SORT_BY'] = 'Trier Par';
+$LANG['DATE_LAST_MODIFIED'] = 'Date de Dernière Modification';
+$LANG['LOCATION_ID'] = 'Identifiant de Emplacement';
+$LANG['LOCALITY'] = 'Localité';
+$LANG['DEC_LAT'] = 'Latitude Décimale';
+$LANG['DEC_LONG'] = 'Longitude Décimale';
+$LANG['ELEV_MIN'] = 'Altitude Minimale';
+$LANG['ELEV_MAX'] = 'Altitude Maximale';
+$LANG['ASCENDING'] = 'ascendant';
+$LANG['DESCENDING'] = 'descendant';
+
+?>

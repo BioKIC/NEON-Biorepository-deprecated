fe9385881e3d13416ae5e16344b15f74de501ac3
Edward Gilbert <egbot@asu.edu>
2021-09-28 18:04:45 -0700

    WordCloud Development

--- collections/specprocessor/wordcloudhandler.php
+++ collections/specprocessor/wordcloudhandler.php
@@ -3,11 +3,8 @@ include_once('../../config/symbini.php');
 include_once($SERVER_ROOT.'/classes/WordCloud.php');
 header("Content-Type: text/html; charset=".$CHARSET);
 
-$collTarget = array_key_exists("colltarget",$_REQUEST)?$_REQUEST["colltarget"]:5;
-
-$cloudHandler = new WordCloud();
-$cloudHandler->setWidth(800);
-$cloudHandler->buildWordFile($collTarget);
+$collidStr = array_key_exists('collidstr',$_REQUEST)?$_REQUEST['collidstr']:false;
+$csMode = array_key_exists('csmode',$_REQUEST)?$_REQUEST['csmode']:false;
 
 ?>
 <html>
@@ -16,26 +13,28 @@ $cloudHandler->buildWordFile($collTarget);
 		<title><?php echo $DEFAULT_TITLE; ?> - Word Cloud Handler Collections</title>
 		<?php
 		$activateJQuery = false;
-		if(file_exists($SERVER_ROOT.'/includes/head.php')){
-			include_once($SERVER_ROOT.'/includes/head.php');
-		}
-		else{
-			echo '<link href="'.$CLIENT_ROOT.'/css/jquery-ui.css" type="text/css" rel="stylesheet" />';
-			echo '<link href="'.$CLIENT_ROOT.'/css/base.css?ver=1" type="text/css" rel="stylesheet" />';
-			echo '<link href="'.$CLIENT_ROOT.'/css/main.css?ver=1" type="text/css" rel="stylesheet" />';
-		}
+		include_once($SERVER_ROOT.'/includes/head.php');
 		?>
 		<script type="text/javascript">
 		</script>
+		<style type="text/css">
+			.url-div{ margin-bottom: 5px; }
+		</style>
 	</head>
 	<body>
 		<!-- This is inner text! -->
 		<div id="innertext">
 			<?php
-			$cloudPath = $CLIENT_ROOT;
-			if(substr($cloudPath,-1) != '/' && substr($cloudPath,-1) != "\\") $cloudPath .= '/';
-			$cloudPath = 'content/collections/wordclouds/ocrcloud'.$collTarget.'.html';
-			echo '<a href="'.$cloudPath.'">Cloud'.$collTarget.'</a>';
+			$cloudHandler = new WordCloud();
+			$cloudHandler->setWidth(800);
+			if(is_numeric($collidStr)){
+				$collidArr = explode(',',$collidStr);
+				foreach($collidArr as $collid){
+					$url = $cloudHandler->buildWordCloud($collid,$csMode);
+					echo '<div class="url-div"><a href="'.$url.'" target="_blank">'.$url.'</a></div>';
+				}
+			}
+			else echo '<div>No collid target submitted</div>';
 			?>
 		</div>
 	</body>

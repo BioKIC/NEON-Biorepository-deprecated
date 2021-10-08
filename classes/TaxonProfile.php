c47b2e0cc00c07e048445364991746934eba86f0
Edward Gilbert <egbot@asu.edu>
2021-10-08 08:43:17 -0700

    TaxonProfile adjustments
    - Minor adjustment to taxon links display

--- classes/TaxonProfile.php
+++ classes/TaxonProfile.php
@@ -471,7 +471,7 @@ class TaxonProfile extends Manager {
 	private function setLinkArr(){
 		if($this->linkArr === false && $this->tid){
 			$sql = 'SELECT DISTINCT l.tlid, l.url, l.icon, l.title, l.notes
-				FROM taxalinks l INNER JOIN taxaenumtree e ON l.tid = e.parenttid
+				FROM taxalinks l LEFT JOIN taxaenumtree e ON l.tid = e.parenttid
 				WHERE (e.tid IN('.$this->tid.') OR l.tid IN('.$this->tid.')) ORDER BY l.sortsequence, l.title';
 			$rs = $this->conn->query($sql);
 			while($r = $rs->fetch_object()){

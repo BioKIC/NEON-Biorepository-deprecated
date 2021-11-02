2d9e6748d40000beb9c96f11679612dcf03c2216
Edward Gilbert <egbot@asu.edu>
2021-11-02 08:59:50 -0700

    Dataset Publishing
    - Minor adjustment to resolve error

--- collections/datasets/datapublisher.php
+++ collections/datasets/datapublisher.php
@@ -281,7 +281,7 @@ include($SERVER_ROOT.'/includes/header.php');
 					<form action="datapublisher.php" method="post" style="display:inline;" onsubmit="return window.confirm('<?php echo $LANG['SURE_DELETE']; ?>');">
 						<input type="hidden" name="colliddel" value="<?php echo $dArr['collid']; ?>">
 						<input type="hidden" name="collid" value="<?php echo $dArr['collid']; ?>">
-						<input type="image" src="../../images/del.png" name="action" value="DeleteCollid" title="<?php echo $LANG['DELETE_ARCHIVE']; ?>" style="width:15px;" />
+						<input type="image" src="../../images/del.png" name="action" value="DeleteCollid" title="<?php echo $LANG['DELETE_ARCHIVE']; ?>" style="width:15px;">
 					</form>
 				</div>
 				<div><b><?php echo $LANG['DESCRIPTION']; ?>:</b> <?php echo $dArr['description']; ?></div>

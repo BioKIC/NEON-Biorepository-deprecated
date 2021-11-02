e6a6d226abed40b951082261bfed2372c4bad81d
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-11-02 08:37:00 -0700

    add Spanish translations to collections/editor/includes

---
+++ content/lang/collections/editor/includes/imagetab.es.php
@@ -0,0 +1,58 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+$LANG['ADD_IMG'] = 'Añadir una Nueva Imagen';
+$LANG['SELECT_IMG'] = 'Seleccionar un archivo de imagen localizado en su computadora que desee subir';
+$LANG['ENTER_URL'] = 'Introducir URL';
+$LANG['ENTER_URL_EXPLAIN'] = 'Introducir una URL a una imagen ya incluída en un servidor web.
+							Si únicamente hay una versión disponible de la imagen, añadir en el campo superior y dejar los otros campos vacíos.
+							Si íconos y versiones medianas no están disponibles, se crearán derivados locales de la imagen.';
+$LANG['IMG_URL'] = 'URL de la imagen (requerido)';
+$LANG['MED_VERS'] = 'Versión Mediana (opcional';
+$LANG['THUMB_VERS'] = 'Versión de Ícono (opcional';
+$LANG['UPLOAD_LOCAL'] = 'Subir Imagen Local';
+$LANG['COPY_TO_SERVER'] = 'Copiar imágenes al servidor (si se deja vacío, se usarán las URL como fuente)';
+$LANG['DO_NOT_MAP_LARGE'] = 'No mapear una versión grande de la imagen (cuando sea aplicable)';
+$LANG['CAPTION'] = 'Descripción';
+$LANG['PHOTOGRAPHER'] = 'Fotógrafo';
+$LANG['SEL_PHOTOG'] = 'Seleccionar Fotógrafo';
+$LANG['DISPLAY_PHOTOG_OVER'] = 'Desplegar campo para sobreescribir fotógrafo';
+$LANG['PHOTOG_OVER'] = 'Fotógrafo (sobreescribir)';
+$LANG['WILL_OVERRIDE'] = 'Advertencia: Sobreescribirá la selección';
+$LANG['NOTES'] = 'Notas';
+$LANG['COPYRIGHT'] = 'Derechos';
+$LANG['SOURCE_WEBPAGE'] = 'Página de Origen';
+$LANG['SORT_SEQUENCE'] = 'Clasificar Secuencia';
+$LANG['DESCRIBE_IMAGE'] = 'Describir esta imagen';
+$LANG['SUBMIT_NEW'] = 'Enviar Nueva Imagen';
+$LANG['OPEN_MED'] = 'Abrir Imagen Mediana';
+$LANG['OPEN_LARGE'] = 'Abrir Imagen Grande';
+$LANG['EDIT_METADATA'] = 'Editar Metadatos de la Imagen';
+$LANG['TAGS'] = 'Etiquetas';
+$LANG['WEB_URL'] = 'URL de la Web';
+$LANG['LARGE_IMG_URL'] = 'URG de Imagen Grande';
+$LANG['THUMB_URL'] = 'URL de Ícono';
+$LANG['EDIT_IMG_DATA'] = 'Editar Datos de la Imagen';
+$LANG['RENAME_FILE'] = 'Renombrar archivo de la imagen web en el servidor para que coincida con la edición anterior';
+$LANG['RENAME_LARGE'] = 'Renombrar archivo de la imagen grande en el servidor para que coincida con la edición anterior';
+$LANG['RENAME_THUMB'] = 'Renombrar archivo de la imagen ícono en el servidor para que coincida con la edición anterior';
+$LANG['SUBMIT_IMG_EDITS'] = 'Enviar Ediciones de Imagen';
+$LANG['DEL_IMG'] = 'Eliminar Imagen';
+$LANG['REM_FROM_SERVER'] = 'Remover imagen del servidor';
+$LANG['RM_DB_NOT_SERVER'] = 'Nota: al no seleccionar se remueve la imagen de la base de datos sin removerla del servidor)';
+$LANG['REMAP_TO_ANOTHER'] = 'Remapear a Otro Espécimen';
+$LANG['OCC_REC_NUM'] = 'Registro de Occurrencia #';
+$LANG['OPEN_LINK_AID'] = 'Abrir Ayuda para Enlace de Ocurrencias';
+$LANG['REMAP_IMG'] = 'Remapear Imagen';
+$LANG['LINK_TO_BLANK'] = 'Enlazar a Nuevo Registro de Ocurrencia en Blanco entre la Colección';
+$LANG['LINK_TO_NEW'] = 'Enlazar a Nueva Ocurrencia';
+$LANG['DISASSOCIATE_IMG_ALL'] = 'Disociar Imagen de todos los Registros de Espécimen';
+$LANG['DISASSOCIATE_IMG'] = 'Disociar Imagen';
+$LANG['IMG_FROM_TAXON'] = 'Imagen estará disponible únicamente en la página del Perfil de Taxón';
+$LANG['MAKE_REQUEST'] = 'Hacer una solicitud de fotografía';
+
+?>

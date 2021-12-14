a20f8b6101defcb0fd9c451b9b592696c3c58e27
Edward Gilbert <egbot@asu.edu>
2021-12-14 13:44:29 -0700

    Geographic Thesaurus development
    - Start of code to harvest geoJson shape definitions from geoBoundaries for selected political boundaries that will be added to geographic thesaurus as a spatial index

--- geothesaurus/harvester.php
+++ geothesaurus/harvester.php
@@ -1,11 +1,10 @@
 <?php
-use ZipStream\Option\Method;
-
 include_once ('../config/symbini.php');
 include_once ($SERVER_ROOT . '/classes/GeographicThesaurus.php');
 header("Content-Type: text/html; charset=".$CHARSET);
 
 $geoThesID = array_key_exists('geoThesID', $_REQUEST) ? $_REQUEST['geoThesID'] : '';
+$gbAction = array_key_exists('gbAction', $_REQUEST) ? $_REQUEST['gbAction'] : '';
 $submitAction = array_key_exists('submitaction', $_POST) ? $_POST['submitaction'] : '';
 
 // Sanitation
@@ -20,11 +19,13 @@ if($IS_ADMIN || array_key_exists('CollAdmin',$USER_RIGHTS)) $isEditor = true;
 $statusStr = '';
 if($isEditor && $submitAction) {
 	if($submitAction == 'transferDataFromLkupTables'){
-		if($geoManager->transferDeprecatedThesaurus()) $statusStr = 'Geographic Lookup data integrated into new Geographic Thesaurus';
-		else $statusStr = implode('<br/>',$geoManager->getWarningArr());
+		if($geoManager->transferDeprecatedThesaurus()) $statusStr = '<span style="color:green;">Geographic Lookup tables transferred into new Geographic Thesaurus</span>';
+		else $statusStr = '<span style="color:green;">'.implode('<br/>',$geoManager->getWarningArr()).'<span style="color:green;">';
+	}
+	elseif($submitAction == 'submitCountryForm'){
+		$geoManager->addPolygon($_POST['geoid'][0]);
 	}
 }
-
 ?>
 <html>
 <head>
@@ -36,10 +37,17 @@ if($isEditor && $submitAction) {
 	<script src="<?php echo $CLIENT_ROOT; ?>/js/jquery.js" type="text/javascript"></script>
 	<script src="<?php echo $CLIENT_ROOT; ?>/js/jquery-ui.js" type="text/javascript"></script>
 	<script type="text/javascript">
-
+		function selectAll(cb){
+			var boxesChecked = true;
+			if(!cb.checked) boxesChecked = false;
+			var f = cb.form;
+			for(var i=0;i<f.length;i++){
+				if(f.elements[i].name == "geoid[]") f.elements[i].checked = boxesChecked;
+			}
+		}
 	</script>
 	<style type="text/css">
-		fieldset{ margin: 10px; padding: 15px; width: 800px }
+		fieldset{ margin: 10px; padding: 15px; }
 		legend{ font-weight: bold; }
 		label{ text-decoration: underline; }
 		#edit-legend{ display: none }
@@ -51,7 +59,7 @@ if($isEditor && $submitAction) {
 		#unitDel-div{ display: none }
 		.button-div{ margin: 15px }
 		.link-div{ margin:0px 30px }
-		#status-div{ margin:15px; padding: 15px; color: red; }
+		#status-div{ margin:15px; padding: 15px; }
 	</style>
 </head>
 <body>
@@ -62,7 +70,7 @@ if($isEditor && $submitAction) {
 	<div class="navpath">
 		<a href="../index.php">Home</a> &gt;&gt;
 		<a href="index.php">Geographic Thesaurus Listing</a> &gt;&gt;
-		<b><a href="index.php">Geographic Harvester</a></b> &gt;&gt;
+		<b>Geographic Harvester</b>
 	</div>
 	<div id='innertext'>
 		<?php
@@ -72,12 +80,13 @@ if($isEditor && $submitAction) {
 
 		if($statusReport = $geoManager->getThesaurusStatus()){
 			$geoRankArr = $geoManager->getGeoRankArr();
-			echo '<fieldset>';
+			echo '<fieldset style="width: 800px">';
 			echo '<legend>Active Geographic Thesaurus</legend>';
 			if(isset($statusReport['active'])){
 				foreach($statusReport['active'] as $geoRank => $cnt){
 					echo '<div><b>'.$geoRankArr[$geoRank].':</b> '.$cnt.'</div>';
 				}
+				echo '<div style="margin-top:20px"><a href="index.php">Goto Geographic Thesaurus</a></div>';
 			}
 			else echo '<div>Active thesaurus is empty</div>';
 			echo '</fieldset>';
@@ -98,6 +107,52 @@ if($isEditor && $submitAction) {
 				</fieldset>
 				<?php
 			}
+			?>
+			<fieldset>
+				<legend>geoBoundaries Harvesting Tools</legend>
+				<ul>
+					<li><a href="harvester.php?gbAction=gbListCountries">List All Countries</a></li>
+				</ul>
+				<?php
+				if($gbAction == 'gbListCountries'){
+					?>
+					<div>
+						<div style="float:right;margin-left:15px"><input name="displayRadio" type="radio" onclick="$('.nopoly').hide();" /> Show no polygon only</div>
+						<div style="float:right;margin-left:15px"><input name="displayRadio" type="radio" onclick="$('.nodb').hide();" /> Show not in database only</div>
+						<div style="float:right;margin-left:15px"><input name="displayRadio" type="radio" onclick="$('.nopoly').show();$('.nodb').show();" /> Show all</div>
+					</div>
+					<form name="" method="post" action="harvester.php">
+						<table class="styledtable">
+							<tr>
+								<th title="Select/Deselect All"><input name="all" type="checkbox" onclick="selectAll(this)" /></th>
+								<th>Name</th><th>ISO</th><th>In Database</th><th>Has Polygon</th><th>ID</th><th>Canonical</th><th>License</th><th>Region</th><th>Full Link</th><th>Preview Image</th>
+							</tr>
+							<?php
+							$countryList = $geoManager->getGBCountryList();
+							foreach($countryList as $iso => $cArr){
+								echo '<tr class="'.(isset($cArr['geoThesID'])?'nodb':'').(isset($cArr['polygon'])?' nopoly':'').'">';
+								echo '<td><input name="geoid[]" type="checkbox" value="'.$cArr['id'].'" '.(isset($cArr['polygon'])?'DISABLED':'').' /></td>';
+								echo '<td>'.$cArr['name'].'</td>';
+								echo '<td>'.$iso.'</td>';
+								echo '<td>'.(isset($cArr['geoThesID'])?'Yes':'No').'</td>';
+								echo '<td>'.(isset($cArr['polygon'])?'Yes':'No').'</td>';
+								echo '<td>'.$cArr['id'].'</td>';
+								echo '<td>'.$cArr['canonical'].'</td>';
+								echo '<td>'.$cArr['license'].'</td>';
+								echo '<td>'.$cArr['region'].'</td>';
+								echo '<td><a href="'.$cArr['link'].'" target="_blank">link</a></td>';
+								echo '<td><a href="'.$cArr['img'].'" target="_blank">IMG</a></td>';
+								echo '</tr>';
+							}
+							?>
+						</table>
+						<button name="submitaction" type="submit" value="submitCountryForm">Submit</button>
+					</form>
+				<?php
+				}
+			?>
+			</fieldset>
+			<?php
 		}
 		?>
 	</div>

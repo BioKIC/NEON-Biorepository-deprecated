7379210c7969098836d9062a55cf9c4ebfb0c065
Edward Gilbert <egbot@asu.edu>
2021-12-29 21:20:55 -0700

    Initial addition of various configuration files

---
+++ includes/head.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Customize styling by adding or modifying CSS file links below
+ * Default styling for individual page is defined within /css/symb/
+ * Individual styling can be customized by:
+ *     1) Uncommenting the $CUSTOM_CSS_PATH variable below
+ *     2) Copying individual CCS file to the /css/symb/custom directory
+ *     3) Modifying the sytle definiation within the file
+ */
+
+//$CUSTOM_CSS_PATH = '/css/symb/custom';
+?>
+<meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />
+<?php
+if($activateJQuery){
+	//For an alternate jQuery UI styling, point link below to another css file
+	echo '<link href="'.$CLIENT_ROOT.'/css/jquery-ui.css" type="text/css" rel="stylesheet">';
+}
+?>
+<link href="<?php echo $CLIENT_ROOT; ?>/css/base.css?ver=1" type="text/css" rel="stylesheet">
+<link href="<?php echo $CLIENT_ROOT; ?>/css/symb/main.css?ver=1" type="text/css" rel="stylesheet">

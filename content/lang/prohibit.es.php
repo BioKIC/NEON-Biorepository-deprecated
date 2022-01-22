b51a0e048e3cdee026f25d60946ab690cb488ba5
Samanta Orellana <kasaore@gmail.com>
2022-01-22 04:36:37 -0700

    Translation to Spanish.

---
+++ content/lang/prohibit.es.php
@@ -0,0 +1,14 @@
+<?php
+/*
+------------------
+Language: Español (Spanish)
+------------------
+*/
+
+$LANG['NO_ACCESS'] = 'Sin Acceso';
+$LANG['PAGE'] = 'Página';
+$LANG['FORBIDDEN'] = 'Prohibida';
+$LANG['NO_PERMISSION'] = 'No tiene permiso para ingresar en esta página';
+$LANG['RETURN'] = 'Regresar a la página de inicioe';
+
+?>

8f0878a0e732e598ef65f2e12a4919784553af67
Samanta Orellana <kasaore@gmail.com>
2022-01-22 11:27:23 -0700

    Translation to Spanish.

---
+++ content/lang/collections/admin/restorebackup.es.php
@@ -0,0 +1,27 @@
+<?php
+/*
+------------------
+Language: Español (Spanish)
+------------------
+*/
+
+$LANG['RESTORE'] = 'Restaurar Respaldo';
+$LANG['PATH_EMPTY'] = 'La dirección al archivo está vacía. Por favor seleccione el archivo que va a ser restaurado.';
+$LANG['MUST_ZIP'] = 'El archivo debe ser un archivo ZIP (.zip) descargado como un archivo de respaldo Symbiota.';
+$LANG['API_SUP'] = 'El archivo API aún no está habilitado en este navegador.';
+$LANG['HOME'] = 'Inicio';
+$LANG['BACKUP_MOD'] = 'Módulo de Restauración de Respaldo';
+$LANG['CAUTION'] = 'Precaución';
+$LANG['MATCH_REPLACE'] = 'Registros que coincidan serán reemplazados con los registros que sean cargados';
+$LANG['SEL_BACKUP'] = 'Seleccionar Archivo de Respaldo para Restaurar';
+$LANG['RESTORE_DETS'] = 'Restaurar Historia de Determinación';
+$LANG['RESTORE_IMGS'] = 'Restaurar Imágenes';
+$LANG['ANALYZE'] = 'Analizar Archivo';
+$LANG['MANUAL'] = 'Opción de Carga Manual de Archivos';
+$LANG['WARNINGS'] = 'Existe advertencia';
+$LANG['LIVE_DANGEROUSLY'] = 'Si piensa que las advertencias son un error, contacte al administrador de su portal';
+$LANG['FINAL_T'] = 'Transferencia final';
+$LANG['DEL_OR_PREV'] = 'Nota: Estos son registros que fueron añadidos luego de que el respaldo fue descargado. Puede ver y eliminar estos registros uno a uno utilizando el enlace de arriba';
+$LANG['OR_CONTACT'] = 'o contacte al administrador de su portal si le gustaría borrar estos registros en lote';
+$LANG['FINALIZE_RESTORE'] = 'Finalizar Restauración';
+?>

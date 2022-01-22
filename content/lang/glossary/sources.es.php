de992dea68d625e28f4ee8fe59abc1f40935225d
Samanta Orellana <kasaore@gmail.com>
2022-01-22 11:06:39 -0700

    Translation to Spanish.

---
+++ content/lang/glossary/sources.es.php
@@ -0,0 +1,21 @@
+<?php
+/*
+------------------
+Language: Español (Spanish)
+------------------
+*/
+
+include_once($SERVER_ROOT.'/content/lang/glossary/index.en.php');
+
+$LANG['G_SOURCES'] = 'Manejo de Fuentes del Glosario';
+$LANG['MAIN_G'] = 'Glosario Principal';
+$LANG['G_CONTR'] = 'Contribuyentes del Glosario';
+$LANG['SAVE'] = 'Guardar Ediciones';
+$LANG['DEL_SRC'] = 'Borrar Fuente';
+$LANG['SURE_DEL'] = '¿Está seguro que quiere borrar esta fuente?';
+$LANG['ADD_SRC'] = 'Añadir Fuente';
+$LANG['CANT_EDIT'] = 'Necesita acceder al portal o tal vez no tiene los permisos necesarios para editar los datos del glosario, por favor contacte al administrador de su portal';
+$LANG['CONTRS'] = 'Contribuyentes';
+$LANG['NO_CONTRS'] = 'Lista de contribuyentes no está disponible';
+
+?>

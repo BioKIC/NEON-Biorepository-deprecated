b25903372f77a3d9f675f9ab3bad6e6969a769ca
Samanta Orellana <kasaore@gmail.com>
2022-01-22 03:51:37 -0700

    Translation to Spanish.

---
+++ content/lang/profile/response.es.php
@@ -0,0 +1,19 @@
+<?php
+/*
+------------------
+Language: Español (Spanish)
+------------------
+*/
+
+$LANG['ANNOTATOR_NAME'] = 'Nombre del Anotador';
+$LANG['ANNOTATOR_EMAIL'] = 'Correo Electrónico del Anotador';
+$LANG['FP_PROBLEM'] = 'El Soporte FilteredPush ha sido habilidato en esta instalación Symbiota, pero el código de ayuda de FilteredPush no ha sido instalado';
+$LANG['NOT_FOUND'] = 'no encontrado';
+$LANG['AGREE'] = 'De acuerdo';
+$LANG['NEUTRAL'] = 'Neutral';
+$LANG['DISAGREE'] = 'En desacuerdo';
+$LANG['OPINION'] = 'Texto de Opinión';
+$LANG['EVIDENCE'] = 'Evidencia';
+$LANG['RESPOND'] = 'Responder';
+
+?>

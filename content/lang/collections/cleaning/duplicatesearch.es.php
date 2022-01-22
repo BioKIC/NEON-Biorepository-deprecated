672fad75cffbc216ce33e41b97d167d8872b88b4
Samanta Orellana <kasaore@gmail.com>
2022-01-21 17:52:01 -0700

    Translation to Spanish.

---
+++ content/lang/collections/cleaning/duplicatesearch.es.php
@@ -0,0 +1,46 @@
+<?php
+/*
+------------------
+Language: Español (Spanish)
+------------------
+*/
+
+$LANG['OCC_CLEANER'] = 'Limpiador de Ocurrencias';
+$LANG['SEL_SPECIMENS'] = '¡Por favor seleccione los especímenes que desea combinar!';
+$LANG['HOME'] = 'Inicio';
+$LANG['COL_MAN'] = 'Manejo de Colecciones';
+$LANG['CLEAN_MOD_INDEX'] = 'Índice del Módulo de Limpieza';
+$LANG['DUP_OCCS'] = 'Ocurrencias Duplicadas';
+$LANG['SUPERADMIN_NOTICE'] = '<span style="color:orange">NOTIFICAR a SuperAdmin:</span>
+					Puede incrementar el límite máximo de número de registros que esta forma puede procesar, incrementando la variable max_input_vars
+					en su archivo PHP de configuración. Incrementar esta variable a 4500 configurará el límite máximo de este formulario a 1000 grupos duplicados.';
+$LANG['DUP_INSTRUCTIONS'] = 'Marque las casillas para seleccionar los registros que le gustaría combinar, and the radio buttons to select which target record to merge into.';
+$LANG['NEXT'] = 'SIGUIENTES';
+$LANG['RECORDS'] = 'REGISTROS';
+$LANG['TO'] = 'a';
+$LANG['DUP_CLUSTERS'] = 'Grupos de Duplicados';
+$LANG['ID'] = 'ID';
+$LANG['SEL_DESEL_ALL'] = 'Seleccionar/Deseleccionar Todo';
+$LANG['BATCH_SWITCH'] = 'Cambiar especímenes destino por lote';
+$LANG['CAT_NUM'] = 'Número de Catálogo';
+$LANG['OTHER_CAT_NUM'] = 'Otros Números de Catálogo';
+$LANG['SCI_NAME'] = 'Nombre Científico';
+$LANG['COLLECTOR'] = 'Colector';
+$LANG['COL_NUM'] = 'Número de Colecta';
+$LANG['ASSOC_COL'] = 'Colectores Asociados';
+$LANG['COL_DATE'] = 'Fecha de Colecta';
+$LANG['VERBAT_DATE'] = 'Fecha Literal';
+$LANG['COUNTRY'] = 'País';
+$LANG['STATE'] = 'Estado';
+$LANG['COUNTY'] = 'Condado';
+$LANG['LOCALITY'] = 'Localidad';
+$LANG['DATE_LAST_MOD'] = 'Última Fecha de Modificación';
+$LANG['MERGE_DUPES'] = 'Combinar Registros Duplicados';
+$LANG['NO_DUPES'] = '¡No hay números de catálogo duplicados!';
+$LANG['DUPE_MERGING_STARTED'] = 'Inició el proceso de combinación de duplicados';
+$LANG['DONE'] = '¡Hecho!';
+$LANG['RETURN_TO_FORM'] = 'Regresar al formulario de combinación de duplicados';
+$LANG['RETURN_TO_MAIN'] = 'Regresar al menú principal';
+$LANG['NOT_AUTH'] = 'No está autorizado para ingresar en esta página';
+
+?>

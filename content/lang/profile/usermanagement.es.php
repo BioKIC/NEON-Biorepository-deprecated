85f430a2a062695c856ee79c3b5c8799d6282016
Samanta Orellana <kasaore@gmail.com>
2022-01-22 04:09:43 -0700

    Translation to Spanish.

---
+++ content/lang/profile/usermanagement.es.php
@@ -0,0 +1,61 @@
+<?php
+/*
+------------------
+Language: Español (Spanish)
+------------------
+*/
+
+$LANG['USER_MNGMT'] = 'Administración de Usuario';
+$LANG['SEARCH'] = 'Ayuda';
+$LANG['LAST_OR_LOGIN'] = 'Apellido o Usuario';
+$LANG['ENTER_LAST'] = 'Introducir Apellido';
+$LANG['QUICK_SEARCH'] = 'Búsqueda Rápida';
+$LANG['TITLE'] = 'Título';
+$LANG['INSTITUTION'] = 'Institución';
+$LANG['CITY'] = 'Ciudad';
+$LANG['STATE'] = 'Estado';
+$LANG['ZIP'] = 'Zip';
+$LANG['COUNTRY'] = 'País';
+$LANG['EMAIL'] = 'Correo Electrónico';
+$LANG['URL'] = 'URL';
+$LANG['USERNAME'] = 'Usuario';
+$LANG['NOT_REGISTERED'] = 'cuenta no registrada para este usuario';
+$LANG['AS_USER'] = 'como este usuario';
+$LANG['PERMISSIONS'] = 'Permisos Actuales';
+$LANG['SUPERADMIN'] = 'Super Administrador';
+$LANG['DEL_PERM'] = 'Borrar Permisos';
+$LANG['TAX_EDITOR'] = 'Editor de Taxonomía';
+$LANG['TAX_PROF_EDITOR'] = 'Editor de Perfil de Taxonomía';
+$LANG['GLOSSARY_EDITOR'] = 'Editor de Glosario';
+$LANG['ID_KEY_ADMIN'] = 'Administrador de Claves de Identificación';
+$LANG['ID_KEY_EDITOR'] = 'Editor de Claves de Identificación';
+$LANG['RARE_SP_ADMIN'] = 'Administrador de Listados de Especies Raras';
+$LANG['RARE_SP_VIEWER'] = 'Ver y Mapear Especímenes de Especies Raras de Todas las Colecciones';
+$LANG['ADMIN_FOR'] = 'Administrador para las siguientes colecciones';
+$LANG['RARE_SP_FOR'] = 'Lector de Especies Sensibles de las siguientes Colecciones';
+$LANG['PERS_OBS_ADMIN'] = 'Administrador de Observaciones Personales';
+$LANG['PERS_OBS_EDITOR'] = 'Editor de Observaciones Personales';
+$LANG['PERS_OBS_RARE_SP_READER'] = 'Lector de Especies Sensibles de Observaciones Personales';
+$LANG['INVENTORY_ADMIN'] = 'Administrador para los siguientes proyectos de inventario';
+$LANG['CHECKLIST_ADMIN'] = 'Administrador para los siguientes listados de especies';
+$LANG['NO_PERMISSIONS'] = 'No han sido asignados permisos para este usuario';
+$LANG['ASSIGN_NEW'] = 'Asignar Nuevos Permisos';
+$LANG['ADD_PERMISSION'] = 'Añadir Permisos';
+$LANG['OCCURRENCE_PROTECT'] = 'Protección de Ocurrencias';
+$LANG['RARE_SP_ADMIN_2'] = 'Administrador de Especies Raras (añadir/remover especies de la lista)';
+$LANG['CAN_READ'] = 'Puede leer Especies Raras de todas las colecciones';
+$LANG['SPEC_COLS'] = 'Colecciones de Especímenes';
+$LANG['ADMIN'] = 'Admin';
+$LANG['EDITOR'] = 'Editor';
+$LANG['RARE'] = 'Raras';
+$LANG['DISABLED'] = 'DESACTIVADO';
+$LANG['COL_ADMIN'] = 'Administrador de Colección';
+$LANG['ABLE_TO_ADD'] = 'Habilitado para añadir y editar datos de especímenes';
+$LANG['ABLE_TO_READ'] = 'Habilitado para leer detalles de especímenes de especies raras';
+$LANG['OBS_PROJECTS'] = 'Proyectos de Observación';
+$LANG['PERS_SP_MGMNT'] = 'Manejo de Especímenes Personales';
+$LANG['INV_MGMNT'] = 'Proyecto de Manejo de Inventario';
+$LANG['CHECKLIST_MGMNT'] = 'Manejo de Listados de Especies';
+$LANG['MUST_LOGIN'] = 'Debe estar conectado y poseer permisos de administrador para manejar usuarios';
+
+?>

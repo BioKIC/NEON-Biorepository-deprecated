fa4b2f370e32ef4355e73910242a61ed7e137031
Edward Gilbert <egbot@asu.edu>
2022-02-08 18:11:19 -0700

    NEON loan check-in issue
    - Add additional identifiers to spreadsheet html display
    - Add additional identifiers csv output
    - Improve management tool layout
    - Add all identifiers as default search for adding specimens to loan
    - Misc bug and layout adjustments

--- collections/loans/reports/defaultspecimenlist.php
+++ collections/loans/reports/defaultspecimenlist.php
@@ -13,7 +13,7 @@ if($collId) $loanManager->setCollId($collId);
 
 $invoiceArr = $loanManager->getInvoiceInfo($loanId,$loanType);
 $addressArr = $loanManager->getFromAddress($collId);
-$specList = $loanManager->getSpecList($loanId);
+$specList = $loanManager->getSpecimenList($loanId);
 $targetCode = $invoiceArr['institutioncode'];
 $sourceCode = $addressArr['institutioncode'];
 if($loanType == 'in'){
@@ -58,7 +58,7 @@ if($outputMode == 'doc'){
 	$table = $section->addTable('listTable');
 	foreach($specList as $specArr){
 		$table->addRow();
-		$table->addCell(2250,$cellStyle)->addText(htmlspecialchars($specArr['catalognumber']),'colFont','colSpace');
+		$table->addCell(2250,$cellStyle)->addText(htmlspecialchars($specArr['catalognumber'].(isset($specArr['othercatalognumbers'])?', '.implode(', '.$specArr['othercatalognumbers']):'')),'colFont','colSpace');
 		$table->addCell(4500,$cellStyle)->addText(htmlspecialchars($specArr['collector']),'colFont','colSpace');
 		$table->addCell(6000,$cellStyle)->addText(htmlspecialchars($specArr['sciname']),'colFont','colSpace');
 	}
@@ -81,14 +81,7 @@ else{
 			<title><?php echo $sourceCode.' '.$invoiceArr['loanidentifierown']; ?> Specimen List</title>
 			<?php
 			$activateJQuery = false;
-			if(file_exists($SERVER_ROOT.'/includes/head.php')){
-				include_once($SERVER_ROOT.'/includes/head.php');
-			}
-			else{
-				echo '<link href="'.$CLIENT_ROOT.'/css/jquery-ui.css" type="text/css" rel="stylesheet" />';
-				echo '<link href="'.$CLIENT_ROOT.'/css/base.css?ver=1" type="text/css" rel="stylesheet" />';
-				echo '<link href="'.$CLIENT_ROOT.'/css/main.css?ver=1" type="text/css" rel="stylesheet" />';
-			}
+			include_once($SERVER_ROOT.'/includes/head.php');
 			?>
 			<style type="text/css">
 				body {font-family:arial,sans-serif;}
@@ -130,7 +123,7 @@ else{
 					<?php
 					foreach($specList as $specArr){
 						echo '<tr>';
-						echo '<td style="width:150px;">'.$specArr['catalognumber'].'</td>';
+						echo '<td style="width:150px;">'.$specArr['catalognumber'].(isset($specArr['othercatalognumbers'])?'<br/>'.implode(', '.$specArr['othercatalognumbers']):'').'</td>';
 						echo '<td style="width:300px;">'.$specArr['collector'].'</td>';
 						echo '<td style="width:400px;">'.$specArr['sciname'].'</td>';
 						echo '<td> </td>';

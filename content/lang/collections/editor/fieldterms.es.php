a5b552179bebb1c9880744f70835abdc5915226a
Edward Gilbert <egbot@asu.edu>
2022-03-30 13:20:41 -0700

    Reverting certain files back to base code

--- content/lang/collections/editor/fieldterms.es.php
+++ content/lang/collections/editor/fieldterms.es.php
@@ -1,11 +1,17 @@
 <?php
 /*
-------------------
-Language: Spanish
-------------------
-*/
+ ------------------
+ Language: Español
+ ------------------
+ */
 
-$LANG['IDNAME'] = 'Nombre de Identificador';
+$LANG['CATALOGNUMBER'] = 'Catalog Number';
+$LANG['IDNAME'] = 'Nombre de Identificador (opcional)';
 $LANG['IDVALUE'] = 'Valor del Identificador Adicional';
+$LANG['RECORDEDBY'] = 'Recorded by';
+$LANG['RECORDNUMBER'] = 'Record number';
+$LANG['EVENTDATE'] = 'Event date';
+$LANG['ASSOCIATEDCOLLECTORS'] = 'Associated collectors';
+$LANG['VERBATIMEVENTDATE'] = 'Verbatim event date';
 
 ?>

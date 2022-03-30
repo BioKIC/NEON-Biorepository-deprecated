827251cb878b245c70eeafde173e30fd3921328a
Edward Gilbert <egbot@asu.edu>
2022-03-30 12:39:03 -0700

    Language file adjustments
    - Add es and fr language files for occurrence editor identifier fields
    - Minor adjustments to language listing in header comment

---
+++ content/lang/collections/editor/fieldterms.es.php
@@ -0,0 +1,17 @@
+<?php
+/*
+------------------
+Language: Español
+------------------
+*/
+
+$LANG['CATALOGNUMBER'] = 'Catalog Number';
+$LANG['IDNAME'] = 'Tag Name (optional)';
+$LANG['IDVALUE'] = 'Additional Identifier Value';
+$LANG['RECORDEDBY'] = 'Recorded by';
+$LANG['RECORDNUMBER'] = 'Record number';
+$LANG['EVENTDATE'] = 'Event date';
+$LANG['ASSOCIATEDCOLLECTORS'] = 'Associated collectors';
+$LANG['VERBATIMEVENTDATE'] = 'Verbatim event date';
+
+?>

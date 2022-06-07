db6cc56ec6675ef7b0a4e63c1434021e9db71c5a
Laura Rocha Prado <lauraprado@asu.edu>
2022-06-07 13:12:54 -0700

    fixes typo in biorepo url

--- includes/citationcoll.php
+++ includes/citationcoll.php
@@ -1,3 +1,3 @@
 <blockquote>
-	<?php echo $collData['collectionname']; ?>. Occurrence dataset (ID: <?php echo $collData['recordid'] ?>) <a href="<?php echo $collData['dwcaurl']; ?>"><?php echo $collData['dwcaurl']; ?></a> accessed via the NEON Biorepository Data Portal, <a href="https//:biorepo.neonscience.org/">http//:biorepo.neonscience.org/</a>, <?php echo date('Y-m-d'); ?>).
+	<?php echo $collData['collectionname']; ?>. Occurrence dataset (ID: <?php echo $collData['recordid'] ?>) <a href="<?php echo $collData['dwcaurl']; ?>"><?php echo $collData['dwcaurl']; ?></a> accessed via the NEON Biorepository Data Portal, <a href="https://biorepo.neonscience.org/">https://biorepo.neonscience.org/</a>, <?php echo date('Y-m-d'); ?>).
 </blockquote>

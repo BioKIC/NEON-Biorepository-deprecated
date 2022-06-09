6cfd2c8678f78f992523e5d9d46f674cbb6bf98c
Laura Rocha Prado <lauraprado@asu.edu>
2022-06-09 12:54:19 -0700

    updates citation format templates (removes link tags)

--- includes/citationdataset.php
+++ includes/citationdataset.php
@@ -1,3 +1,3 @@
 <blockquote>
-	Biodiversity occurrence data published by: NEON (National Ecological Observatory Network) Biorepository, Arizona State University, Biodiversity Knowledge Integration Center. <?php echo $dArr['name']; ?> (ID: <?php echo $datasetid; ?>) https://biorepo.neonscience.org/portal/collections/list.php?datasetid=<?php echo $datasetid; ?> accessed via the NEON Biorepository Data Portal, <a href="https://biorepo.neonscience.org/">https://biorepo.neonscience.org/</a>, <?php echo date('Y-m-d'); ?>).
+	Biodiversity occurrence data published by: NEON (National Ecological Observatory Network) Biorepository, Arizona State University, Biodiversity Knowledge Integration Center. <?php echo $dArr['name']; ?> (ID: <?php echo $datasetid; ?>) https://biorepo.neonscience.org/portal/collections/list.php?datasetid=<?php echo $datasetid; ?> accessed via the NEON Biorepository Data Portal, https://biorepo.neonscience.org/, <?php echo date('Y-m-d'); ?>).
 </blockquote>

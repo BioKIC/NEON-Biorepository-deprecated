3bd4e30e8e27d6007d6243df3088f50951dbfeaf
Laura Rocha Prado <lauraprado@asu.edu>
2022-06-21 15:10:10 -0700

    renames citationcoll to citationcollection

--- includes/citationcoll.php
+++
@@ -1,3 +0,0 @@
-<blockquote>
-	<?php echo $collData['collectionname']; ?>. Occurrence dataset (ID: <?php echo $collData['recordid'] ?>) <?php echo $collData['dwcaurl']; ?> accessed via the NEON Biorepository Data Portal, https://biorepo.neonscience.org/, <?php echo date('Y-m-d'); ?>).
-</blockquote>
---
+++ includes/citationcollection.php
@@ -0,0 +1,1 @@
+	<?php echo $collData['collectionname']; ?>. Occurrence dataset (ID: <?php echo $collData['recordid'] ?>) <?php echo $collData['dwcaurl']; ?> accessed via the NEON Biorepository Data Portal, https://biorepo.neonscience.org/, <?php echo date('Y-m-d'); ?>).

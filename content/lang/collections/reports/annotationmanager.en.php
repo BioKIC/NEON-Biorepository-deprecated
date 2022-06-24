6afa34e1cf29a3dc813ca12e0d6bb323648311d9
Katie Pearson <katelin.d.pearson24@gmail.com>
2022-06-24 13:47:41 -0700

    Fix sharedterms reference

--- content/lang/collections/reports/annotationmanager.en.php
+++ content/lang/collections/reports/annotationmanager.en.php
@@ -5,10 +5,10 @@ Language: English
 ------------------
 */
 
-include_once('sharedterms.en.php');
+include_once($SERVER_ROOT.'content/lang/collections/sharedterms.en.php');
 
 $LANG['ANN_LAB_MAN'] = 'Annotation Label Manager';
-$LANG[SEL_ANN] = 'Please select at least one annotation record!';
+$LANG['SEL_ANN'] = 'Please select at least one annotation record!';
 
 
 ?>

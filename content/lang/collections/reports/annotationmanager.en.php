beeab8456c2c0024761aed631dedf262d6928f6a
Katie Pearson <katelin.d.pearson24@gmail.com>
2022-06-24 13:49:31 -0700

    Update annotationmanager.en.php

--- content/lang/collections/reports/annotationmanager.en.php
+++ content/lang/collections/reports/annotationmanager.en.php
@@ -5,7 +5,7 @@ Language: English
 ------------------
 */
 
-include_once($SERVER_ROOT.'content/lang/collections/sharedterms.en.php');
+include_once($SERVER_ROOT.'/content/lang/collections/sharedterms.en.php');
 
 $LANG['ANN_LAB_MAN'] = 'Annotation Label Manager';
 $LANG['SEL_ANN'] = 'Please select at least one annotation record!';

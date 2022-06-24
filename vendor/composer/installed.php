eb1226d50b6ff84c77508333183af2192eb9fa3e
Greg Post <gregory.post@asu.edu>
2022-06-24 15:58:08 -0500

    phpoffice cleanup
    re-installing phpoffice using composer to resolve duplicate folder structure due to case in-senstive OS

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => 'edf93d19f95f953507cecca58f2f79368b228029',
         'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'edf93d19f95f953507cecca58f2f79368b228029',
             'dev_requirement' => false,
         ),
         'ezyang/htmlpurifier' => array(

255313c5fedc9b802eb2a8516cb08c9491460c57
Edward Gilbert <egbot@asu.edu>
2022-08-03 13:47:44 -0700

    Minor but significant bug fix to previous submit

--- classes/UtilityFunctions.php
+++ classes/UtilityFunctions.php
@@ -5,7 +5,7 @@
 
 class UtilityFunctions {
 
-	private static function getDomain(){
+	public static function getDomain(){
 		$domain = 'http://';
 		if((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443) $domain = 'https://';
 		if(!empty($GLOBALS['SERVER_HOST'])){

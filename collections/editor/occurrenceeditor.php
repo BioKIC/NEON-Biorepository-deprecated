329ea0844cd712571a754eac006c3ec53fd9eb59
Katie Pearson <katelin.d.pearson24@gmail.com>
2022-08-04 07:03:25 -0700

    Fix field hover-over text, lang tag spacing issue

--- collections/editor/occurrenceeditor.php
+++ collections/editor/occurrenceeditor.php
@@ -1346,7 +1346,7 @@ else{
 													<a href="#" onclick="return dwcDoc('occurrenceid')" tabindex="-1"><img class="docimg" src="../../images/qmark.png" /></a><br/>
 													<input type="text" name="occurrenceid" maxlength="255" value="<?php echo array_key_exists('occurrenceid',$occArr)?$occArr['occurrenceid']:''; ?>" onchange="fieldChanged('occurrenceid');" />
 												</div>
-												<div id="fieldNumberDiv" title="If different than institution code">
+												<div id="fieldNumberDiv" title="An identifier given to the collecting event in the field">
 													<?php echo (defined('FIELDNUMBERLABEL')?FIELDNUMBERLABEL:'Field Number'); ?>
 													<a href="#" onclick="return dwcDoc('fieldnumber')" tabindex="-1"><img class="docimg" src="../../images/qmark.png" /></a><br/>
 													<input type="text" name="fieldnumber" maxlength="45" value="<?php echo array_key_exists('fieldnumber',$occArr)?$occArr['fieldnumber']:''; ?>" onchange="fieldChanged('fieldnumber');" />

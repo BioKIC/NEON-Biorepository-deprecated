7e6b7a0266091b1f02a0321c5d84ac992ba2b055
Katie Pearson <katelin.d.pearson24@gmail.com>
2022-08-04 14:08:37 -0700

    Lang tags for taxonomy files

---
+++ content/lang/taxa/taxonomy/taxonomydisplay.en.php
@@ -0,0 +1,29 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+$LANG['TAX_DISPLAY'] = 'Taxonomy Display';
+$LANG['HOME'] = 'Home';
+$LANG['TAX_TREE_VIEWER'] = 'Taxonomic Tree Viewer';
+$LANG['ADD_NEW_TAXON'] = 'Add a New Taxon';
+$LANG['DESCRIPTION'] = 'Description';
+$LANG['EDITORS'] = 'Editors';
+$LANG['CONTACT'] = 'Contact';
+$LANG['EMAIL'] = 'Email';
+$LANG['NOTES'] = 'Notes';
+$LANG['TAX_SEARCH'] = 'Taxon Search';
+$LANG['TAXON'] = 'Taxon';
+$LANG['DISP_TAX_TREE'] = 'Display Taxon Tree';
+$LANG['DISP_AUTHORS'] = 'Display authors';
+$LANG['MATCH_WHOLE_WORDS'] = 'Match on whole words';
+$LANG['DISP_FULL_TREE'] = 'Display full tree below family';
+$LANG['DISP_SUBGENERA'] = 'Display species with subgenera';
+
+//Dynamic display
+$LANG['TAX_EXPLORE'] = 'Taxonomy Explorer';
+$LANG['EDITOR_MODE'] = 'Editor mode';
+
+?>

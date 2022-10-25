4d318f348404236724864dbd37b2e1215c7bb36b
Laura Rocha Prado <lauraprado@asu.edu>
2022-10-25 12:10:57 -0300

    adds page specific css file checker to head

--- includes/head_template.php
+++ includes/head_template.php
@@ -7,6 +7,9 @@
 ** css folder and include them here.
 */
 $CSS_PATH = $CLIENT_ROOT . '/css/v' . ($CSS_VERSION_RELEASE ? $CSS_VERSION_RELEASE : 'legacy');
+$parent = $_SERVER['PHP_SELF'];
+$pageSpecificCSS = str_replace('.php', '.css', $parent);
+$pageSpecificCSS = str_replace($CLIENT_ROOT, '', $pageSpecificCSS);
 ?>
 <!-- Responsive viewport -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,3 +17,8 @@ $CSS_PATH = $CLIENT_ROOT . '/css/v' . ($CSS_VERSION_RELEASE ? $CSS_VERSION_RELEA
 <!-- Symbiota styles -->
 <link href="<?php echo $CSS_PATH; ?>/symbiota/normalize.slim.css" type="text/css" rel="stylesheet">
 <link href="<?php echo $CSS_PATH; ?>/symbiota/main.css?ver=1" type="text/css" rel="stylesheet">
+
+<!-- Page Specific styles -->
+<?php if (file_exists($_SERVER['DOCUMENT_ROOT'] . $CSS_PATH . '/symbiota' . $pageSpecificCSS)) {
+	echo '<link href="' . $CSS_PATH . '/symbiota' . $pageSpecificCSS . '" type="text/css" rel="stylesheet">';
+} ?>

32bb803d5978fb4cb23c3da53b74dc4bab0ed16f
Samanta Orellana <kasaore@gmail.com>
2022-11-28 18:00:46 -0700

    Translation to Spanish

---
+++ content/lang/collections/misc/sharedterms.es.php
@@ -0,0 +1,35 @@
+<?php
+/*
+------------------
+Language: Español
+------------------
+*/
+
+$LANG['HOME'] = 'Inicio';
+$LANG['COLL_PROFILE'] = 'Perfil de Colección';
+$LANG['QUICK_SEARCH'] = 'Búsqueda Rápida';
+$LANG['CAT_NUM'] = 'Número de Catálogo';
+$LANG['COL_MGMNT'] = 'Administración de Colecciones';
+$LANG['MAILING_ADD'] = 'Dirección Postal';
+$LANG['NEED_INST_CODE'] = 'El Código de Institución debe tener un valor';
+$LANG['NEED_COLL_VALUE'] = 'Nombre de Colección debe tener un valor';
+$LANG['CANNOT_GUID'] = 'El GUID Generado por Symbiota no puede ser seleccionado para una colección que es manejada localmente fuera de este portal (e.g. manejo tipo Snapshot). En este caso, el GUID debe ser generado en la base de datos de origen e ingresado en el portal como parte del proceso de importación de datos.';
+$LANG['NEED_DECIMAL'] = 'Los valores de latitud y longitud deben estar en formato decimal (solamente valores numéricos)';
+$LANG['NEED_RIGHTS'] = 'El campo de Derechos (e.g. licencia Creative Commons) debe tener una opción seleccionada';
+$LANG['SORT_NUMERIC'] = 'La secuencia de orden debe ser solamente numérica';
+$LANG['AGG_GUID'] = 'Un conjunto de datos Agregado (e.g. especímenes provenientes de múltiples colecciones) pueden tener únicamente occurrenceID seleccionado como fuente de los GUID';
+$LANG['NEED_GUID'] = 'Debe seleccionar una fuente de GUID para poder publicar a agregadores de datos.';
+$LANG['SEL_INST'] = 'Seleccione una institución para vincular';
+$LANG['NOT_SUPP'] = 'El archivo que ha importado no es un formato de imagen permitido. Por favor seleccione un archivo jpg, png, o gif.';
+$LANG['MUST_SMALL'] = 'El archivo de imagen debe ser menor a 350 pixeles de ancho y alto.';
+$LANG['NOT_SUPP_URL'] = 'La url que ingresó no corresponde a un formato permitido de imagen. Por favor introduzca una url para un archivo jpg, png, o gif.';
+$LANG['EDIT_ADDRESS'] = 'Editar dirección institucional';
+$LANG['UNLINK_ADDRESS'] = 'Desvincular dirección institucional';
+$LANG['NO_ADDRESS'] = 'No hay direcciones vinculadas';
+$LANG['SEL_ADDRESS'] = 'Seleccionar Dirección Institucional';
+$LANG['LINK_ADDRESS'] = 'Vincular Dirección';
+$LANG['ADD_ADDRESS'] = 'Añadir una nueva dirección no incluida en la lista';
+$LANG['ADD_INST'] = 'Añadir una nueva institución no incluida en la lista';
+$LANG['LINK_URL_REQ'] = 'URL de enlace y título requeridos';
+
+?>

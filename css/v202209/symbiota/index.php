429fbb9d271b3f055e11e5eb4672fd516f73b426
Edward Gilbert <egbot@asu.edu>
2022-12-12 21:35:38 -0700

    Styling Adjustments
    - Remove bootstrap css since it is no longer used within Symbiota
    - Remove selective css from v202209 directory since they will soon be targeted for removal. css files will only selectively added to the version directory
    - Add and adjust index.php redirect files

--- css/v202209/symbiota/index.php
+++ css/v202209/symbiota/index.php
@@ -1,6 +1,6 @@
 <?php
-include_once('../config/symbini.php');
-header('Content-Type: text/html; charset=' . $CHARSET);
+include_once('../../../config/symbini.php');
+header('Content-Type: text/html; charset='.$CHARSET);
 header('Location: '.$CLIENT_ROOT.'/index.php');
 ?>
 <html>

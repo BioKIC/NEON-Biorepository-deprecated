1f51f570697a52c51461777f2c0edf783878827c
Katie Pearson <katelin.d.pearson24@gmail.com>
2023-01-11 08:11:59 -0800

    Lang tags for imageloader.php

---
+++ content/lang/imagelib/admin/imageloader.en.php
@@ -0,0 +1,34 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+$LANG['IMG_LOADER'] = 'Image Loader';
+$LANG['HOME'] = 'Home';
+$LANG['IMG_IMPORTER'] = 'Image Importer';
+$LANG['IMG_UPLOAD_FORM'] = 'Image Upload Form';
+$LANG['IMG_UPLOAD_EXPLAIN'] = 'This tool is designed to aid collection managers in batch importing image files
+					that are defined within a comma delimited text file (CSV). The only two required fields are
+					the image url. If scientific name is null, script will attempt to extract taxon name from image file name.
+					The image urls must represent the full path to the image, or consist of the file names with base path
+					defined within the ingestion form.
+					Other optional fields include: photographer, caption, locality, sourceUrl, anatomy,
+					notes, collection identifier, owner, copyright, sortSequence.
+					Internal fields can include photographerUid, occid, or tid.';
+$LANG['UPLOAD_FILE'] = 'Upload File';
+$LANG['ANALYZE_INPUT_FILE'] = 'Analyze Input File';
+$LANG['SOURCE_FIELD'] = 'Source Field';
+$LANG['TARGET_FIELD'] = 'Target Field';
+$LANG['SELECT_TARGET'] = 'Select Target';
+$LANG['LEAVE_UNMAPPED'] = 'Leave Field Unmapped';
+$LANG['FIELDS_YELLOW'] = 'Fields in yellow are not yet mapped or verified';
+$LANG['LRG_IMG'] = 'Large Image';
+$LANG['LEAVE_BLANK'] = 'Leave blank';
+$LANG['MAP_REMOTE_IMGS'] = 'Map to remote images';
+$LANG['IMPORT_LOCAL'] = 'Import to local storage';
+$LANG['BASE_PATH'] = 'Base Path';
+$LANG['UPLOAD_IMGS'] = 'Upload Images';
+
+?>
